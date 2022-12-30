fd645247b1265d6ce58b12410f89a896d3a1471d
SUZUKI Kei <todokimasen@gmail.com>
2022-12-30 23:55:42 +0900

    絶対パスである必要が無かったので相対パスに変更した.

--- src/test/daily-coding.bashrc.test.sh
+++ src/test/daily-coding.bashrc.test.sh
@@ -19,40 +19,40 @@ function test._daily-coding._locate-file
 
     declare -ar files=(
         # 日付が連続する場合.
-        "${TEST_DATA_DIR}/2022-01-08/aaa/file.txt"
-        "${TEST_DATA_DIR}/2022-01-09/aaa/file.txt"
-        "${TEST_DATA_DIR}/2022-01-10/aaa/file.txt"
-        "${TEST_DATA_DIR}/2022-01-11/aaa/file.txt"
-        "${TEST_DATA_DIR}/2022-01-12/aaa/file.txt"
+        '2022-01-08/aaa/file.txt'
+        '2022-01-09/aaa/file.txt'
+        '2022-01-10/aaa/file.txt'
+        '2022-01-11/aaa/file.txt'
+        '2022-01-12/aaa/file.txt'
 
         # 日付が連続しない場合.
-        "${TEST_DATA_DIR}/2022-02-07/aaa/file.txt"
-        "${TEST_DATA_DIR}/2022-02-09/aaa/file.txt"
-        "${TEST_DATA_DIR}/2022-02-10/aaa/file.txt"
-        "${TEST_DATA_DIR}/2022-02-11/aaa/file.txt"
-        "${TEST_DATA_DIR}/2022-02-13/aaa/file.txt"
+        '2022-02-07/aaa/file.txt'
+        '2022-02-09/aaa/file.txt'
+        '2022-02-10/aaa/file.txt'
+        '2022-02-11/aaa/file.txt'
+        '2022-02-13/aaa/file.txt'
 
         # 全ての作業ディレクトリに同一のファイルが存在しない場合.
-        "${TEST_DATA_DIR}/2022-03-06/aaa/file-1.txt"
-        "${TEST_DATA_DIR}/2022-03-07/aaa/file-2.txt"
-        "${TEST_DATA_DIR}/2022-03-08/aaa/file-1.txt"
-        "${TEST_DATA_DIR}/2022-03-09/aaa/file-2.txt"
-        "${TEST_DATA_DIR}/2022-03-10/aaa/file-1.txt"
-        "${TEST_DATA_DIR}/2022-03-11/aaa/file-2.txt"
-        "${TEST_DATA_DIR}/2022-03-12/aaa/file-1.txt"
-        "${TEST_DATA_DIR}/2022-03-13/aaa/file-2.txt"
-        "${TEST_DATA_DIR}/2022-03-14/aaa/file-1.txt"
+        '2022-03-06/aaa/file-1.txt'
+        '2022-03-07/aaa/file-2.txt'
+        '2022-03-08/aaa/file-1.txt'
+        '2022-03-09/aaa/file-2.txt'
+        '2022-03-10/aaa/file-1.txt'
+        '2022-03-11/aaa/file-2.txt'
+        '2022-03-12/aaa/file-1.txt'
+        '2022-03-13/aaa/file-2.txt'
+        '2022-03-14/aaa/file-1.txt'
 
         # 全ての作業ディレクトリに同一のディレクトリが存在しない場合.
-        "${TEST_DATA_DIR}/2022-04-06/aaa/file.txt"
-        "${TEST_DATA_DIR}/2022-04-07/bbb/file.txt"
-        "${TEST_DATA_DIR}/2022-04-08/aaa/file.txt"
-        "${TEST_DATA_DIR}/2022-04-09/bbb/file.txt"
-        "${TEST_DATA_DIR}/2022-04-10/aaa/file.txt"
-        "${TEST_DATA_DIR}/2022-04-11/bbb/file.txt"
-        "${TEST_DATA_DIR}/2022-04-12/aaa/file.txt"
-        "${TEST_DATA_DIR}/2022-04-13/bbb/file.txt"
-        "${TEST_DATA_DIR}/2022-04-14/aaa/file.txt"
+        '2022-04-06/aaa/file.txt'
+        '2022-04-07/bbb/file.txt'
+        '2022-04-08/aaa/file.txt'
+        '2022-04-09/bbb/file.txt'
+        '2022-04-10/aaa/file.txt'
+        '2022-04-11/bbb/file.txt'
+        '2022-04-12/aaa/file.txt'
+        '2022-04-13/bbb/file.txt'
+        '2022-04-14/aaa/file.txt'
     )
 
     for file in "${files[@]}"; do
