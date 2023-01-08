0d169ba15cefdbf77612f0b0f56c3ab19d6ccb5b
SUZUKI Kei <todokimasen@gmail.com>
2023-01-08 12:02:12 +0900

    fizz-buzz.forth

---
+++ workspace/2023-01-08/fizz-buzz.forth/fizz-buzz.fs
@@ -0,0 +1,34 @@
+
+: fizz-buzz-value ( n -- )
+    dup 15 mod 0 = if
+        s" FizzBuzz " type drop
+    else
+        dup 5 mod 0 = if
+            s" Buzz " type drop
+        else
+            dup 3 mod 0 = if
+                s" Fizz " type drop
+            else
+                .
+            endif
+        endif
+    endif
+;
+
+: _fizz-buzz recursive ( max min -- )
+    over over = if
+        drop drop           ( max min -- )
+    else
+        dup fizz-buzz-value
+        1 +                 ( max min -- max min+1 )
+        _fizz-buzz
+    endif
+;
+
+: fizz-buzz ( n -- )
+    1           ( n -- n 1 )
+    _fizz-buzz
+;
+
+100 fizz-buzz
+
