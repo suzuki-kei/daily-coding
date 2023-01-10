a8a04509934c0b3c4daeebe6b6875a2a9b0efb88
SUZUKI Kei <todokimasen@gmail.com>
2023-01-10 23:42:24 +0900

    bubble-sort.c

---
+++ workspace/2023-01-10/bubble-sort.c/main.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define SIZE 20
+
+void initialize();
+void set_random_values(int *values, int size);
+void print_values(const int *values, int size);
+int is_sorted(const int *values, int size);
+void bubble_sort(int *values, int size);
+void swap(int *values, int index1, int index2);
+
+int main(void)
+{
+    int values[SIZE];
+
+    initialize();
+    set_random_values(values, SIZE);
+    print_values(values, SIZE);
+    bubble_sort(values, SIZE);
+    print_values(values, SIZE);
+
+    return 0;
+}
+
+void initialize()
+{
+    srand(time(NULL));
+}
+
+void set_random_values(int *values, int size)
+{
+    int i;
+
+    for (i = 0; i < size; i++)
+        values[i] = rand() % 90 + 10;
+}
+
+void print_values(const int *values, int size)
+{
+    int i;
+
+    for (i = 0; i < size; i++)
+        printf("%d ", values[i]);
+
+    if (is_sorted(values, size))
+        printf("(sorted)\n");
+    else
+        printf("(not sorted)\n");
+}
+
+int is_sorted(const int *values, int size)
+{
+    int i;
+
+    for (i = 0; i < size - 1; i++)
+        if (values[i] > values[i + 1])
+            return 0;
+    return 1;
+}
+
+void bubble_sort(int *values, int size)
+{
+    int i, upper;
+
+    for (upper = size; upper > 1; upper--)
+        for (i = 0; i < upper; i++)
+            if (values[i] > values[i + 1])
+                swap(values, i, i + 1);
+}
+
+void swap(int *values, int index1, int index2)
+{
+    const int value1 = values[index1];
+    values[index1] = values[index2];
+    values[index2] = value1;
+}
+
