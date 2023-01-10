5865211c445a1fc867ca5c64b6dfc544e97b6204
SUZUKI Kei <todokimasen@gmail.com>
2023-01-10 22:12:46 +0900

    quick-sort.scheme

---
+++ workspace/2023-01-10/quick-sort.scheme/quick-sort.scm
@@ -0,0 +1,57 @@
+(use srfi-27)
+
+(define main
+    (lambda (arguments)
+        (random-source-randomize!
+            default-random-source)
+        (let ((xs (generate-random-values 20)))
+            (print xs)
+            (print (quick-sort xs)))
+        0))
+
+(define generate-random-values
+    (lambda (n)
+        (map
+            (lambda (_)
+                (+ (random-integer 90) 10))
+            (iota n))))
+
+(define quick-sort
+    (lambda (xs)
+        (cond
+            ((null? xs)
+                '())
+            (else
+                (receive
+                    (less-xs equal-xs greater-xs)
+                    (quick-sort-partition (car xs) xs)
+                    (append
+                        (quick-sort less-xs)
+                        equal-xs
+                        (quick-sort greater-xs)))))))
+
+(define quick-sort-partition
+    (lambda (pivot xs)
+        (quick-sort-partition.tailrec
+            '() '() '() pivot xs)))
+
+(define quick-sort-partition.tailrec
+    (lambda (less-xs equal-xs greater-xs pivot xs)
+        (cond
+            ((null? xs)
+                (values less-xs equal-xs greater-xs))
+            (else
+                (quick-sort-partition.tailrec
+                    (cons-if < (car xs) pivot less-xs)
+                    (cons-if = (car xs) pivot equal-xs)
+                    (cons-if > (car xs) pivot greater-xs)
+                    pivot
+                    (cdr xs))))))
+
+(define cons-if
+    (lambda (test? x pivot xs)
+        (if
+            (test? x pivot)
+            (cons x xs)
+            xs)))
+
