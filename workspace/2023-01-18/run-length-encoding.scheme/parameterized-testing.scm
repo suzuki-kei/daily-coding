8a8a5a2783875f1452455519dc64ff2df9455138
SUZUKI Kei <todokimasen@gmail.com>
2023-01-18 20:25:27 +0900

    run-length-encoding.scheme

---
+++ workspace/2023-01-18/run-length-encoding.scheme/parameterized-testing.scm
@@ -0,0 +1,20 @@
+
+(define test.parameterized
+    (lambda (callback tuples)
+        (test.parameterized.tailrec
+            callback tuples 0)))
+
+(define test.parameterized.tailrec
+    (lambda (callback tuples index)
+        (cond
+            ((null? tuples)
+                #f)
+            (else
+                (apply
+                    callback
+                    (cons index (car tuples)))
+                (test.parameterized.tailrec
+                    callback
+                    (cdr tuples)
+                    (+ index 1))))))
+
