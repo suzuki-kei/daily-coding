5de8283ae0a326d7c49ccc732911e29c6a752122
SUZUKI Kei <todokimasen@gmail.com>
2023-01-26 07:23:46 +0900

    parameterized-testing.python

---
+++ workspace/2023-01-26/parameterized-testing.python/parameterized.py
@@ -0,0 +1,35 @@
+import inspect
+import unittest
+
+
+def parameterized(tuples):
+    def new_wrapper(method, tuple):
+        def wrapper(self):
+            method(self, *tuple)
+        return wrapper
+
+    def decorator(method):
+        for i, tuple in enumerate(tuples):
+            wrapper = new_wrapper(method, tuple)
+            wrapper_name = f"{method.__name__}_{i}"
+            f_locals = inspect.currentframe().f_back.f_locals
+            f_locals[wrapper_name] = wrapper
+
+    return decorator
+
+
+class TestCase(unittest.TestCase):
+
+    @parameterized([
+        ( 3, 1, 2),
+        ( 7, 3, 4),
+        (11, 5, 6),
+        ( 0, 7, 8),
+    ])
+    def test(self, expected, a, b):
+        self.assertEqual(expected, a + b)
+
+
+if __name__ == "__main__":
+    unittest.main()
+
