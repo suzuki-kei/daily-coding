84c2021d1d75beb3c11812f959e85522518e45d2
SUZUKI Kei <todokimasen@gmail.com>
2023-01-28 13:46:05 +0900

    try-prolog.prolog

---
+++ workspace/2023-01-28/try-prolog.prolog/main.pl
@@ -0,0 +1,22 @@
+
+minimum(X, Y, X) :- X =< Y, !.
+minimum(X, Y, Y) :- X >= Y, !.
+
+maximum(X, Y, X) :- X >= Y, !.
+maximum(X, Y, Y) :- X =< Y, !.
+
+odd(1) :- true, !.
+odd(N) :- N < 0, is(M, -N), odd(M), !.
+odd(N) :- N > 2, is(M, N-2), odd(M), !.
+
+even(0) :- true, !.
+even(N) :- N < 0, is(M, -N), even(M), !.
+even(N) :- N > 1, is(M, N-2), even(M), !.
+
+abs(N, X) :- N >= 0, is(X, N), !.
+abs(N, X) :- N =< 0, is(X, -N), !.
+
+negate(N, X) :- is(X, -N).
+
+square(N, X) :- is(X, N*N).
+
