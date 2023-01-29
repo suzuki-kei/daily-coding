7cf1964139fbe9832268474205d850e1c7500a86
SUZUKI Kei <todokimasen@gmail.com>
2023-01-29 20:59:36 +0900

    selection-sort.c

---
+++ workspace/2023-01-29/selection-sort.c/main.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define SIZE 20
+
+void initialize();
+void set_random_values(int *values, int size);
+void print_values(const int *values, int size);
+int is_sorted(const int *values, int size);
+void selection_sort(int *values, int size);
+void swap(int *values, int index1, int index2);
+
+int main(void)
+{
+    int values[SIZE];
+
+    initialize();
+    set_random_values(values, SIZE);
+    print_values(values, SIZE);
+    selection_sort(values, SIZE);
+    print_values(values, SIZE);
+
+    return 0;
+}
+
+void initialize()
+{
+    srand(time(NULL));
+}
+
+void set_random_values(int *values, int size)
+{
+    int i;
+
+    for (i = 0; i < size; i++)
+        values[i] = rand() % 90 + 10;
+}
+
+void print_values(const int *values, int size)
+{
+    int i;
+
+    for (i = 0; i < size; i++)
+        printf("%d ", values[i]);
+
+    if (is_sorted(values, size))
+        printf("(sorted)\n");
+    else
+        printf("(not sorted)\n");
+}
+
+int is_sorted(const int *values, int size)
+{
+    int i;
+
+    for (i = 0; i < size - 1; i++)
+        if (values[i] > values[i + 1])
+            return 0;
+    return 1;
+}
+
+void selection_sort(int *values, int size)
+{
+    int i;
+    int target_index;
+    int selected_index;
+
+    for (target_index = 0; target_index < size - 1; target_index++)
+    {
+        for (i = selected_index = target_index; i < size; i++)
+            if (values[i] < values[selected_index])
+                selected_index = i;
+        swap(values, target_index, selected_index);
+    }
+}
+
+void swap(int *values, int index1, int index2)
+{
+    const int value1 = values[index1];
+    values[index1] = values[index2];
+    values[index2] = value1;
+}
+
