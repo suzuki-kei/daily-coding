35036583b6a470e9aa881427b5ecfc268713a534
SUZUKI Kei <todokimasen@gmail.com>
2023-01-31 07:05:17 +0900

    fibonacci-number.prolog

---
+++ workspace/2023-01-31/fibonacci-number.prolog/fibonacci-number.pl
@@ -0,0 +1,12 @@
+
+fibonacci(0, 0) :- true, !.
+fibonacci(1, 1) :- true, !.
+fibonacci(N, Fn) :-
+    N > 1,
+    is(A, N-2),
+    is(B, N-1),
+    fibonacci(A, Fa),
+    fibonacci(B, Fb),
+    is(Fn, Fa + Fb),
+    !.
+
