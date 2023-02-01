5d7a54eab42ce3a9c705253f4683d80e038f744f
SUZUKI Kei <todokimasen@gmail.com>
2023-02-02 07:06:45 +0900

    merge-sort.scheme

---
+++ workspace/2023-02-02/merge-sort.scheme/merge-sort.scm
@@ -0,0 +1,63 @@
+(use srfi-27)
+
+(define main
+    (lambda (arguments)
+        (random-source-randomize!
+            default-random-source)
+        (let ((xs (generate-random-values 20)))
+            (print xs)
+            (print (merge-sort xs)))
+        0))
+
+(define generate-random-values
+    (lambda (n)
+        (map
+            (lambda (_)
+                (+ (random-integer 90) 10))
+            (iota n))))
+
+(define merge-sort
+    (lambda (xs)
+        (cond
+            ((null? xs)
+                xs)
+            ((null? (cdr xs))
+                xs)
+            (else
+                (merge-sort-merge
+                    (merge-sort-merge
+                        (take xs 1)
+                        (take (drop xs 1) 1))
+                    (merge-sort
+                        (drop xs 2)))))))
+
+(define merge-sort-merge
+    (lambda (xs ys)
+        (merge-sort-merge.tailrec
+            '() xs ys)))
+
+(define merge-sort-merge.tailrec
+    (lambda (merged xs ys)
+        (cond
+            ((or (null? xs) (null? ys))
+                (reverse
+                    (append
+                        (reverse xs)
+                        (reverse ys)
+                        merged)))
+            ((< (car xs) (car ys))
+                (merge-sort-merge.tailrec
+                    (cons (car xs) merged)
+                    (cdr xs)
+                    ys))
+            ((> (car xs) (car ys))
+                (merge-sort-merge.tailrec
+                    (cons (car ys) merged)
+                    xs
+                    (cdr ys)))
+            (else
+                (merge-sort-merge.tailrec
+                    (cons* (car xs) (car ys) merged)
+                    (cdr xs)
+                    (cdr ys))))))
+
