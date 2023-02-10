15fae750cc9b0dde09e566050d44fb4c73b22e20
SUZUKI Kei <todokimasen@gmail.com>
2023-02-10 23:51:34 +0900

    run-length-encoding.ruby

---
+++ workspace/2023-02-10/run-length-encoding.ruby/run_length_encoding.rb
@@ -0,0 +1,54 @@
+
+def encode(xs)
+    runs = xs_to_runs(xs)
+    runs_to_encoded_runs(runs)
+end
+
+def runs_to_encoded_runs(runs)
+    runs.map do |run|
+        run_to_encoded_run(run)
+    end
+end
+
+def run_to_encoded_run(run)
+    [run[0], run.size]
+end
+
+def xs_to_runs(xs)
+    if xs.empty?
+        return []
+    end
+
+    runs, run, xs = [], xs[0..0], xs[1..-1]
+
+    xs.each do |x|
+        if x == run[0]
+            run.append(x)
+        else
+            runs.append(run)
+            run = [x]
+        end
+    end
+
+    runs.append(run)
+end
+
+def decode(encoded_runs)
+    runs = encoded_runs_to_runs(encoded_runs)
+    runs_to_xs(runs)
+end
+
+def encoded_runs_to_runs(encoded_runs)
+    encoded_runs.map do |encoded_run|
+        encoded_run_to_run(encoded_run)
+    end
+end
+
+def encoded_run_to_run(encoded_run)
+    [encoded_run[0]] * encoded_run[1]
+end
+
+def runs_to_xs(runs)
+    runs.reduce([], :+)
+end
+
