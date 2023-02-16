cddd547ec7459d204d76d201e9fabb3d784abe67
SUZUKI Kei <todokimasen@gmail.com>
2023-02-16 21:14:16 +0900

    fizz-buzz.c

---
+++ workspace/2023-02-16/fizz-buzz.c/fizz-buzz.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+void fizz_buzz(int n);
+
+int main(void)
+{
+    fizz_buzz(100);
+
+    return 0;
+}
+
+void fizz_buzz(int n)
+{
+    int i;
+
+    for (i = 1; i <= n; i++)
+        if (i % 15 == 0)
+            printf("FizzBuzz\n");
+        else if (i % 5 == 0)
+            printf("Buzz\n");
+        else if (i % 3 == 0)
+            printf("Fizz\n");
+        else
+            printf("%d\n", i);
+}
+
