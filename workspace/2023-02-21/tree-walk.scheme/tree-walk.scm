574ea32b7d9961cd3f378bb375e4218f13af46fe
SUZUKI Kei <todokimasen@gmail.com>
2023-02-21 07:12:19 +0900

    tree-walk.scheme

---
+++ workspace/2023-02-21/tree-walk.scheme/tree-walk.scm
@@ -0,0 +1,45 @@
+
+(define-syntax trace
+    (syntax-rules ()
+        ((_ expression)
+            (begin
+                (print "==== " 'expression)
+                (print expression)))))
+
+(define main
+    (lambda (arguments)
+        (let ((tree '(a (b) ((c d) e))))
+            (trace (tree-walk for-each print tree atom?))
+            (trace (tree-walk (reversed for-each) print tree atom?))
+            (trace (tree-walk map identity tree atom?))
+            (trace (tree-walk (reversed map) identity tree atom?)))
+        0))
+
+(define tree-walk
+    (lambda (walker procedure tree leaf?)
+        (walker
+            (lambda (x)
+                (cond
+                    ((leaf? x)
+                        (procedure x))
+                    (else
+                        (tree-walk walker procedure x leaf?))))
+            tree)))
+
+(define atom?
+    (lambda (x)
+        (and
+            (not (null? x))
+            (not (pair? x)))))
+
+(define identity
+    (lambda (x)
+        x))
+
+(define reversed
+    (lambda (walker)
+        (lambda (f tree)
+            (walker
+                f
+                (reverse tree)))))
+
