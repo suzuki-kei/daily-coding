1097fb304c892b8fa79d33a49be840b1b49e2dcf
SUZUKI Kei <todokimasen@gmail.com>
2023-02-27 07:13:12 +0900

    factorial.rust

---
+++ workspace/2023-02-27/factorial.rust/factorial.rs
@@ -0,0 +1,17 @@
+
+fn main()
+{
+    for n in 1..10 {
+        println!("factorial({}) = {}", n, factorial(n));
+    }
+}
+
+fn factorial(n: i32) -> i32
+{
+    if n == 0 {
+        1
+    } else {
+        n * factorial(n - 1)
+    }
+}
+
