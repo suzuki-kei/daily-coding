e647261792590b266b068292a16a5869b8771ee5
SUZUKI Kei <todokimasen@gmail.com>
2023-03-06 07:02:06 +0900

    fizz-buzz.haskell

---
+++ workspace/2023-03-06/fizz-buzz.haskell/fizz-buzz.hs
@@ -0,0 +1,14 @@
+
+main :: IO ()
+main = print $ fizzBuzz 100
+
+fizzBuzz :: Int -> [String]
+fizzBuzz n = map fizzBuzzValue [1..n]
+
+fizzBuzzValue :: Int -> String
+fizzBuzzValue n
+    | n `mod` 15 == 0 = "FizzBuzz"
+    | n `mod`  5 == 0 = "Buzz"
+    | n `mod`  3 == 0 = "Fizz"
+    | otherwise       = show n
+
