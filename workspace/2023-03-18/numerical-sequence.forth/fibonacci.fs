9eb2aee9b57d6a9f077102818185e6ae660803d7
SUZUKI Kei <todokimasen@gmail.com>
2023-03-18 22:34:57 +0900

    numerical-sequence.forth

---
+++ workspace/2023-03-18/numerical-sequence.forth/fibonacci.fs
@@ -0,0 +1,32 @@
+
+: _fibonacci recursive ( a b n -- fn )
+    dup 0 = if
+        drop drop   ( a b n -- a )
+    else
+        1 -         ( a b n -- a b n-1 )
+        rot rot     ( a b n-1 -- n-1 a b )
+        tuck +      ( n-1 a b -- n-1 b a+b )
+        rot         ( n-1 b a+b -- b a+b n-1 )
+        _fibonacci  ( b a+b n-1 -- fn )
+    endif
+;
+
+: fibonacci ( n -- fn )
+    0 1 rot     ( n -- 0 1 n )
+    _fibonacci  ( 0 1 n -- fn )
+;
+
+: _show_fibonacci_numbers recursive ( max min -- )
+    over over < if
+        drop drop
+    else
+        dup fibonacci .
+        1 + _show_fibonacci_numbers
+    endif
+;
+
+: show_fibonacci_numbers ( max min -- )
+    1
+    _show_fibonacci_numbers
+;
+
---
+++ workspace/2023-03-18/numerical-sequence.forth/main.fs
@@ -0,0 +1,9 @@
+
+s\" factorials:\n" type
+10 show_factorials
+s\" \n\n" type
+
+s\" fibonacci numbers:\n" type
+20 show_fibonacci_numbers
+s\" \n\n" type
+
