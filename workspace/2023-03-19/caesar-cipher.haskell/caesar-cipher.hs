fe9d1ed5e6729eb77bb547ec180bbde60fbfc104
SUZUKI Kei <todokimasen@gmail.com>
2023-03-19 10:09:38 +0900

    caesar-cipher.haskell

---
+++ workspace/2023-03-19/caesar-cipher.haskell/caesar-cipher.hs
@@ -0,0 +1,31 @@
+import Data.Char
+import Text.Printf
+
+main :: IO ()
+main = demonstration "Hello, World!" [1..26]
+
+demonstration :: String -> [Int] -> IO ()
+demonstration _ [] = pure ()
+demonstration s (n:ns) = do
+    let encoded = encode n s
+    let decoded = decode n encoded
+    printf "(n = %2d) %s -> %s -> %s\n" n s encoded decoded
+    demonstration s ns
+
+encode :: Int -> String -> String
+encode n s = map (rotate n) s
+
+decode :: Int -> String -> String
+decode n s = encode (26 - n) s
+
+rotate :: Int -> Char -> Char
+rotate n c
+    | isLower c = rotateLower n c
+    | isUpper c = rotateUpper n c
+    | otherwise = rotateNonAlphabet n c
+    where
+        rotateLower = rotate $ ord 'a'
+        rotateUpper = rotate $ ord 'A'
+        rotateNonAlphabet n c = c
+        rotate offset n c = chr $ (ord c + n - offset) `mod` 26 + offset
+
