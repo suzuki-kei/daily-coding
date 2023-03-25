8ffe2f0516b17bd86f16f9b14f2888c5fc8e6f4c
SUZUKI Kei <todokimasen@gmail.com>
2023-03-25 09:53:47 +0900

    numerical-sequence.forth

---
+++ workspace/2023-03-25/numerical-sequence.forth/factorial.fs
@@ -0,0 +1,33 @@
+
+: _factorial recursive ( fn n -- fn )
+    dup 0 = if
+        drop        ( fn n -- fn )
+    else
+        swap        ( fn n -- n fn )
+        over *      ( n fn -- n fn*n )
+        swap        ( n fn*n -- fn*n n )
+        1 -         ( fn*n n -- fn*n n-1 )
+        _factorial  ( fn*n n-1 -- fn )
+    endif
+;
+
+: factorial ( n -- fn )
+    1 swap      ( n -- 1 n )
+    _factorial  ( 1 n -- fn )
+;
+
+: _show_factorials recursive ( max min -- )
+    over over < if
+        drop drop           ( max min -- )
+    else
+        dup factorial .
+        1 +                 ( max min -- max min+1 )
+        _show_factorials    ( max min+1 -- )
+    endif
+;
+
+: show_factorials ( n -- )
+    1                   ( n -- n 1 )
+    _show_factorials    ( n 1 -- )
+;
+
