6f48ebdb9dd8fa3c56b0e78d36246d426cd374bb
SUZUKI Kei <todokimasen@gmail.com>
2023-04-03 22:51:03 +0900

    numerical-sequence.erlang

---
+++ workspace/2023-04-03/numerical-sequence.erlang/main.erl
@@ -0,0 +1,36 @@
+
+main(_) ->
+    io:format("factorials:~n"),
+    lists:foreach(fun(N) -> io:format("~p ", [factorial(N)]) end, lists:seq(0, 10)),
+    io:format("~n"),
+
+    io:format("fibonacci numbers:~n"),
+    lists:foreach(fun(N) -> io:format("~p ", [fibonacci(N)]) end, lists:seq(0, 20)),
+    io:format("~n"),
+
+    io:format("FizzBuzz values:~n"),
+    lists:foreach(fun(N) -> io:format("~s ", [fizzbuzz(N)]) end, lists:seq(0, 100)),
+    io:format("~n"),
+
+    ok.
+
+factorial(0) ->
+    1;
+factorial(N) ->
+    N * factorial(N - 1).
+
+fibonacci(0) ->
+    0;
+fibonacci(1) ->
+    1;
+fibonacci(N) ->
+    fibonacci(N - 1) + fibonacci(N - 2).
+
+fizzbuzz(N) ->
+    if
+        N rem 15 == 0 -> "FizzBuzz";
+        N rem  5 == 0 -> "Buzz";
+        N rem  3 == 0 -> "Fizz";
+        true          -> integer_to_list(N)
+    end.
+
