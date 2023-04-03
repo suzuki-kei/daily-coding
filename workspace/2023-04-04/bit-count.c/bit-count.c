10aaa880542c1571ca3d8e3e3d87791b3dc5f571
SUZUKI Kei <todokimasen@gmail.com>
2023-04-04 07:07:08 +0900

    bit-count.c

---
+++ workspace/2023-04-04/bit-count.c/bit-count.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int bit_count(int value);
+
+int main(void)
+{
+    int i;
+
+    for (i = 0; i <= 32; i++)
+        printf("bit_count(%d) = %d\n", i, bit_count(i));
+
+    return 0;
+}
+
+int bit_count(int value)
+{
+    value = (value & 0x55555555) + (value >>  1 & 0x55555555);
+    value = (value & 0x33333333) + (value >>  2 & 0x33333333);
+    value = (value & 0x0F0F0F0F) + (value >>  4 & 0x0F0F0F0F);
+    value = (value & 0x00FF00FF) + (value >>  8 & 0x00FF00FF);
+    value = (value & 0x0000FFFF) + (value >> 16 & 0x0000FFFF);
+    return value;
+}
+
