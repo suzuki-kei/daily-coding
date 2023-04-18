1cdceddac8af5b71290b206f0a80fe70ec73a90b
SUZUKI Kei <todokimasen@gmail.com>
2023-04-19 08:18:52 +0900

    set.shell

---
+++ workspace/2023-04-19/set.shell/set.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -eu
+
+function set1
+{
+    seq 10 16
+}
+
+function set2
+{
+    seq 14 20
+}
+
+function to_line
+{
+    cat | tr '\n' ' '
+}
+
+echo "set1 = $(set1 | to_line)"
+echo "set2 = $(set2 | to_line)"
+echo "union(set1, set2) = $(sort -u <(set1) <(set2) | to_line)"
+echo "difference(set1, set2) = $(sort <(set1) <(set2) <(set2) | uniq -u | to_line)"
+echo "intersection(set1, set2) = $(sort <(set1) <(set2) | uniq -d | to_line)"
+
