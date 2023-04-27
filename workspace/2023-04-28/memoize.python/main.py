62ce8423d2f4d2083b6f644218186a1663bf8aff
SUZUKI Kei <todokimasen@gmail.com>
2023-04-28 07:07:24 +0900

    memoize.python

---
+++ workspace/2023-04-28/memoize.python/main.py
@@ -0,0 +1,17 @@
+from memoize import memoize
+
+def main():
+    for n in range(100):
+        print(f"fibonacci({n}) = {fibonacci(n)}")
+
+
+@memoize
+def fibonacci(n):
+    if n in (0, 1):
+        return n
+    return fibonacci(n - 1) + fibonacci(n - 2)
+
+
+if __name__ == "__main__":
+    main()
+
