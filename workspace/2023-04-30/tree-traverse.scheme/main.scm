51bf23a5f05a6b1e4a469f1530f8ad1c882ca752
SUZUKI Kei <todokimasen@gmail.com>
2023-04-30 16:46:51 +0900

    tree-traverse.scheme

---
+++ workspace/2023-04-30/tree-traverse.scheme/main.scm
@@ -0,0 +1,19 @@
+(load "traverse")
+
+(define-syntax trace
+    (syntax-rules ()
+        ((_ x)
+            (begin
+                (print "==== " 'x)
+                (print x)))))
+
+(define main
+    (lambda (arguments)
+        (let ((tree '(a (b) ((c d) e))))
+            (trace tree)
+            (trace (traverse for-each print atom? tree))
+            (trace (traverse (reversed for-each) print atom? tree))
+            (trace (traverse map identity atom? tree))
+            (trace (traverse (reversed map) identity atom? tree)))
+        0))
+
