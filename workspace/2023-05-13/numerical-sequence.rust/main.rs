138fc4fae923fca3387f130fe33936e298c42873
SUZUKI Kei <todokimasen@gmail.com>
2023-05-13 17:32:45 +0900

    numerical-sequence.rust

--- workspace/2023-05-13/numerical-sequence.rust/main.rs
+++ workspace/2023-05-13/numerical-sequence.rust/main.rs
@@ -18,32 +18,28 @@ fn show_numerical_sequence<T: std::fmt::Display>(
 
 fn factorial(n: i32) -> i32
 {
-    if n == 0 {
-        1
-    } else {
-        n * factorial(n - 1)
+    match n {
+        0 => 1,
+        _ => n * factorial(n - 1),
     }
 }
 
 fn fibonacci(n: i32) -> i32
 {
-    if n == 0 || n == 1 {
-        n
-    } else {
-        fibonacci(n - 1) + fibonacci(n - 2)
+    match n {
+        0 => 0,
+        1 => 1,
+        _ => fibonacci(n - 1) + fibonacci(n - 2),
     }
 }
 
 fn fizzbuzz(n: i32) -> String
 {
-    if n % 15 == 0 {
-        "FizzBuzz".to_string()
-    } else if n % 5 == 0 {
-        "Buzz".to_string()
-    } else if n % 3 == 0 {
-        "Fizz".to_string()
-    } else {
-        n.to_string()
+    match (n % 5, n % 3) {
+        (0, 0) => "FizzBuzz".to_string(),
+        (0, _) => "Buzz".to_string(),
+        (_, 0) => "Fizz".to_string(),
+        (_, _) => n.to_string(),
     }
 }
 
