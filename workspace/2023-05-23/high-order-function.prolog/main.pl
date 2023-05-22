05dbba679a12c6053a108ea112a8ac2137e063d3
SUZUKI Kei <todokimasen@gmail.com>
2023-05-23 07:10:52 +0900

    high-order-function.prolog

---
+++ workspace/2023-05-23/high-order-function.prolog/main.pl
@@ -0,0 +1,38 @@
+
+main :-
+    range(0, 10, Xs),
+
+    format("values: "),
+    foreach(member(X, Xs), format("~d ", [X])), nl,
+
+    format("odd values: "),
+    foreach(find(X, Xs, odd), format("~d ", [X])), nl,
+
+    format("even values: "),
+    foreach(find(X, Xs, even), format("~d ", [X])), nl,
+
+    halt.
+
+odd(X) :-
+    X mod 2 =:= 1.
+
+even(X) :-
+    X mod 2 =:= 0.
+
+range(Min, Max, []) :-
+    Min > Max.
+
+range(Min, Max, [Min|Xs]) :-
+    Min =< Max,
+    is(NextMin, Min + 1),
+    range(NextMin, Max, Xs).
+
+find(X, [Head|Tail], Predicate) :-
+    find(X, Head, Tail, Predicate).
+
+find(X, X, _, Predicate) :-
+    call(Predicate, X).
+
+find(X, _, [Head|Tail], Predicate) :-
+    find(X, Head, Tail, Predicate).
+
