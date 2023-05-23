9864171281e034d578390ba65a0ac4333527b2ff
SUZUKI Kei <todokimasen@gmail.com>
2023-05-23 22:08:57 +0900

    numerical-sequence.forth

---
+++ workspace/2023-05-23/numerical-sequence.forth/factorial.fs
@@ -0,0 +1,30 @@
+
+: _factorial recursive ( fn n -- fn )
+    dup 0 = if
+        drop
+    else
+        tuck * swap
+        1 - _factorial
+    endif
+;
+
+: factorial ( n -- fn )
+    1 swap
+    _factorial
+;
+
+: _show_factorials recursive ( max min -- )
+    over over < if
+        drop drop
+    else
+        dup factorial .
+        1 + _show_factorials
+    endif
+;
+
+: show_factorials ( n -- )
+    s\" factorials:\n" type
+    1 _show_factorials
+    s\" \n" type
+;
+
---
+++ workspace/2023-05-23/numerical-sequence.forth/fibonacci.fs
@@ -0,0 +1,31 @@
+
+: _fibonacci recursive ( a b n -- fn )
+    dup 0 = if
+        drop drop
+    else
+        1 - rot rot
+        tuck + rot
+        _fibonacci
+    endif
+;
+
+: fibonacci ( n -- fn )
+    0 1 rot
+    _fibonacci
+;
+
+: _show_fibonacci_numbers recursive ( max min -- )
+    over over < if
+        drop drop
+    else
+        dup fibonacci .
+        1 + _show_fibonacci_numbers
+    endif
+;
+
+: show_fibonacci_numbers ( n -- )
+    s\" fibonacci numbers:\n" type
+    0 _show_fibonacci_numbers
+    s\" \n" type
+;
+
