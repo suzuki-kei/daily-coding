bfed5a60fd85c723007252d48abb048df362be07
SUZUKI Kei <todokimasen@gmail.com>
2023-06-04 10:05:33 +0900

    parameterized-testing.python

---
+++ workspace/2023-06-04/parameterized-testing.python/test.py
@@ -0,0 +1,19 @@
+import parameterized
+import unittest
+
+
+class TestCase(unittest.TestCase):
+
+    @parameterized.parameterized([
+        ( 3, 1, 2),
+        ( 7, 3, 4),
+        (11, 5, 6),
+        (15, 7, 8),
+    ])
+    def test_add(self, expected, a, b):
+        self.assertEqual(expected, a + b)
+
+
+if __name__ == "__main__":
+    unittest.main()
+
