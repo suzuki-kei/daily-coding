59958f3b83d92ca7b33eae98b86a466eefd04ee1
SUZUKI Kei <todokimasen@gmail.com>
2023-06-20 00:19:43 +0900

    quick-sort.c

---
+++ workspace/2023-06-20/quick-sort.c/main.c
@@ -0,0 +1,65 @@
+#include "quick-sort.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define SIZE 20
+
+void initialize();
+void set_random_values(int *values, int size);
+void print_array(const int *values, int size);
+int is_sorted(const int *values, int size);
+
+int main(void)
+{
+    int values[SIZE];
+
+    initialize();
+    set_random_values(values, SIZE);
+    print_array(values, SIZE);
+    quick_sort(values, SIZE);
+    print_array(values, SIZE);
+
+    return 0;
+}
+
+void initialize()
+{
+    srand(time(NULL));
+}
+
+void set_random_values(int *values, int size)
+{
+    int i;
+
+    for (i = 0; i < size; i++)
+        values[i] = rand() % 90 + 10;
+}
+
+void print_array(const int *values, int size)
+{
+    int i;
+    const char *delimiter = "";
+
+    for (i = 0; i < size; i++)
+    {
+        printf("%s%d", delimiter, values[i]);
+        delimiter = ", ";
+    }
+
+    if (is_sorted(values, size))
+        printf(" (sorted)\n");
+    else
+        printf(" (not sorted)\n");
+}
+
+int is_sorted(const int *values, int size)
+{
+    int i;
+
+    for (i = 0; i < size - 1; i++)
+        if (values[i] > values[i + 1])
+            return 0;
+    return 1;
+}
+
