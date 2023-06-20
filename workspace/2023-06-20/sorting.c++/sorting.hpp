e2274817d8931359fcb8762c6e1d79effbdd2389
SUZUKI Kei <todokimasen@gmail.com>
2023-06-20 22:31:32 +0900

    sorting.c++

---
+++ workspace/2023-06-20/sorting.c++/sorting.hpp
@@ -0,0 +1,42 @@
+#ifndef SORTING_HPP_INCLUDED
+#define SORTING_HPP_INCLUDED
+
+#include <algorithm>
+
+template
+<
+    typename T
+>
+void quick_sort(T *values, int lower, int upper)
+{
+    int lower_index = lower;
+    int upper_index = upper;
+    const T pivot = values[(lower + upper) / 2];
+
+    while (lower_index <= upper_index)
+    {
+        while (values[lower_index] < pivot)
+            lower_index++;
+        while (pivot < values[upper_index])
+            upper_index--;
+        if (lower_index <= upper_index)
+            std::swap(values[lower_index++], values[upper_index--]);
+    }
+
+    if (lower < upper_index)
+        quick_sort(values, lower, upper_index);
+    if (lower_index < upper)
+        quick_sort(values, lower_index, upper);
+}
+
+template
+<
+    typename T
+>
+void quick_sort(T *values, int size)
+{
+    quick_sort(values, 0, size - 1);
+}
+
+#endif
+
