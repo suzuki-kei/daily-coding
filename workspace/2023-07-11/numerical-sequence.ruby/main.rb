7799091aead704258e47408fcc2707bdfce888ce
SUZUKI Kei <todokimasen@gmail.com>
2023-07-11 08:27:56 +0900

    numerical-sequence.ruby

---
+++ workspace/2023-07-11/numerical-sequence.ruby/main.rb
@@ -0,0 +1,45 @@
+
+def main
+    print_sequence('factorials:', method(:factorial), 1, 10)
+    print_sequence('fibonacci numbers:', method(:fibonacci), 0, 20)
+    print_sequence('fizz buzz values:', method(:fizz_buzz), 0, 100)
+end
+
+def print_sequence(description, f, min, max)
+    puts description
+    puts (min..max).map(&f).join(' ')
+end
+
+def factorial(n)
+    case n
+        when 0
+            1
+        else
+            n * factorial(n - 1)
+    end
+end
+
+def fibonacci(n)
+    case n
+        when 0, 1
+            n
+        else
+            fibonacci(n - 1) + fibonacci(n - 2)
+    end
+end
+
+def fizz_buzz(n)
+    case [n % 5 == 0, n % 3 == 0]
+        when [true, true]
+            'FizzBuzz'
+        when [true, false]
+            'Buzz'
+        when [false, true]
+            'Fizz'
+        else
+            n.to_s
+    end
+end
+
+main
+
