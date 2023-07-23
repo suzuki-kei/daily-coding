41e26e6cc7f1c1d592868d2142a874cfbc130cb6
SUZUKI Kei <todokimasen@gmail.com>
2023-07-24 07:55:33 +0900

    flatten.python

--- workspace/2023-07-24/flatten.python/flatten.py
+++ workspace/2023-07-24/flatten.python/flatten.py
@@ -1,6 +1,31 @@
 import collections.abc
 
 
+# 再帰版
+def flatten(values):
+    flatten_values = []
+
+    for value in values:
+        if is_iterable(value):
+            flatten_values.extend(flatten(value))
+        else:
+            flatten_values.append(value)
+    return flatten_values
+
+
+# 再帰版 (1 要素ずつ処理する)
+def flatten(values):
+    def flatten(x):
+        if x == []:
+            return []
+        if not is_iterable(x):
+            return [x]
+        return flatten(x[0]) + flatten(x[1:])
+
+    return flatten(values)
+
+
+# 非再帰版
 def flatten(values):
     values = values.copy()
 
