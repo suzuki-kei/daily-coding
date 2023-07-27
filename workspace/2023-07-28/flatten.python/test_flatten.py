0c080649d6a4559fe0abc45be8b0421189d28c5e
SUZUKI Kei <todokimasen@gmail.com>
2023-07-28 00:28:37 +0900

    flatten.python

---
+++ workspace/2023-07-28/flatten.python/test_flatten.py
@@ -0,0 +1,17 @@
+import unittest
+from flatten import *
+
+
+class TestCase(unittest.TestCase):
+
+    def test_flatten(self):
+        self.assertEqual([], flatten([]))
+        self.assertEqual([1], flatten([1]))
+        self.assertEqual([], flatten([[]]))
+        self.assertEqual([1, 2, 3, 4, 5], flatten([[1], [2], [3], [4], [5]]))
+        self.assertEqual([1, 2, 3, 4, 5], flatten([1, [2], [[3, 4], 5]]))
+
+
+if __name__ == "__main__":
+    unittest.main()
+
