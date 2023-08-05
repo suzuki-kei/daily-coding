a827b28cb84acb47822294a39b154d5fe4a0fda9
SUZUKI Kei <todokimasen@gmail.com>
2023-08-05 10:56:01 +0900

    flatten.scheme

---
+++ workspace/2023-08-05/flatten.scheme/flatten.scm
@@ -0,0 +1,25 @@
+
+(define atom?
+    (lambda (x)
+        (and
+            (not (null? x))
+            (not (pair? x)))))
+
+(define flatten?
+    (lambda (xs)
+        (every atom? xs)))
+
+(define flatten
+    (lambda (xs)
+        (cond
+            ((null? xs)
+                xs)
+            ((atom? (car xs))
+                (cons
+                    (car xs)
+                    (flatten (cdr xs))))
+            (else
+                (append
+                    (flatten (car xs))
+                    (flatten (cdr xs)))))))
+
