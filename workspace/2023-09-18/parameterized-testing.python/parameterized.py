a7b15ca8b27536636b20916aa60edb3ba3218b04
SUZUKI Kei <todokimasen@gmail.com>
2023-09-18 17:40:48 +0900

    parameterized-testing.python

---
+++ workspace/2023-09-18/parameterized-testing.python/parameterized.py
@@ -0,0 +1,28 @@
+import inspect
+
+
+def expand(tuples):
+
+    def decorator(parameterized_method):
+        f_locals = inspect.currentframe().f_back.f_locals
+        f_locals |= to_expanded_method_map(parameterized_method, tuples)
+
+    def to_expanded_method_map(parameterized_method, tuples):
+        return {
+            expanded_method.__name__: expanded_method
+            for expanded_method in to_expanded_methods(parameterized_method, tuples)
+        }
+
+    def to_expanded_methods(parameterized_method, tuples):
+        return [
+            to_expanded_method(parameterized_method, tuple, index)
+            for index, tuple in enumerate(tuples)
+        ]
+
+    def to_expanded_method(parameterized_method, tuple, index):
+        expanded_method = lambda self: parameterized_method(self, *tuple)
+        expanded_method.__name__ = f"{parameterized_method.__name__}_{index + 1}"
+        return expanded_method
+
+    return decorator
+
