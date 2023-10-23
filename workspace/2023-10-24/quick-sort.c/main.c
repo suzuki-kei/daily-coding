129947ac5ccf32fde978b67912db9800523e34df
SUZUKI Kei <todokimasen@gmail.com>
2023-10-24 07:06:56 +0900

    quick-sort.c

---
+++ workspace/2023-10-24/quick-sort.c/main.c
@@ -0,0 +1,108 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define SIZE 20
+
+void initialize(void);
+void demonstration(void);
+void set_random_values(int *values, int size);
+void print_values(const int *values, int size);
+int is_sorted(const int *values, int size);
+void quick_sort(int *values, int size);
+void quick_sort_range(int *values, int lower, int upper);
+void swap(int *values, int index1, int index2);
+
+int main(void)
+{
+    initialize();
+    demonstration();
+
+    return 0;
+}
+
+void initialize(void)
+{
+    srand(time(NULL));
+}
+
+void demonstration(void)
+{
+    int values[SIZE];
+
+    set_random_values(values, SIZE);
+    print_values(values, SIZE);
+    quick_sort(values, SIZE);
+    print_values(values, SIZE);
+}
+
+void set_random_values(int *values, int size)
+{
+    int i;
+
+    for (i = 0; i < size; i++)
+        values[i] = rand() % 90 + 10;
+}
+
+void print_values(const int *values, int size)
+{
+    int i;
+    const char *delimiter = "";
+
+    for (i = 0; i < size; i++)
+    {
+        printf("%s%d", delimiter, values[i]);
+        delimiter = " ";
+    }
+
+    if (is_sorted(values, size))
+        printf(" (sorted)\n");
+    else
+        printf(" (not sorted)\n");
+}
+
+int is_sorted(const int *values, int size)
+{
+    int i;
+
+    for (i = 0; i < size - 1; i++)
+        if (values[i] > values[i + 1])
+            return 0;
+
+    return 1;
+}
+
+void quick_sort(int *values, int size)
+{
+    quick_sort_range(values, 0, size - 1);
+}
+
+void quick_sort_range(int *values, int lower, int upper)
+{
+    int lower_index = lower;
+    int upper_index = upper;
+    const int pivot = values[(lower + upper) / 2];
+
+    while (lower_index <= upper_index)
+    {
+        while (values[lower_index] < pivot)
+            lower_index++;
+        while (values[upper_index] > pivot)
+            upper_index--;
+        if (lower_index <= upper_index)
+            swap(values, lower_index++, upper_index--);
+    }
+
+    if (lower < upper_index)
+        quick_sort_range(values, lower, upper_index);
+    if (lower_index < upper)
+        quick_sort_range(values, lower_index, upper);
+}
+
+void swap(int *values, int index1, int index2)
+{
+    const int value1 = values[index1];
+    values[index1] = values[index2];
+    values[index2] = value1;
+}
+
