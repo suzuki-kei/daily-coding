0077580e79ceeee764a5c0d19c72cb9f3f7a93a9
SUZUKI Kei <todokimasen@gmail.com>
2023-11-05 09:17:19 +0900

    quick-sort.ruby

---
+++ workspace/2023-11-05/quick-sort.ruby/main.rb
@@ -0,0 +1,40 @@
+
+def main
+    values = generate_random_values(20)
+    puts values.join(' ')
+    quick_sort(values)
+    puts values.join(' ')
+end
+
+def generate_random_values(n)
+    n.times.map do
+        rand(90) + 10
+    end
+end
+
+def quick_sort(values)
+    quick_sort_range(values, 0, values.size - 1)
+end
+
+def quick_sort_range(values, lower, upper)
+    lower_index = lower
+    upper_index = upper
+    pivot = values[(lower + upper) / 2]
+
+    while lower_index <= upper_index
+        lower_index += 1 while values[lower_index] < pivot
+        upper_index -= 1 while values[upper_index] > pivot
+
+        if lower_index <= upper_index
+            values[lower_index], values[upper_index] = values[upper_index], values[lower_index]
+            lower_index += 1
+            upper_index -= 1
+        end
+    end
+
+    quick_sort_range(values, lower, upper_index) if lower < upper_index
+    quick_sort_range(values, lower_index, upper) if lower_index < upper
+end
+
+main
+
