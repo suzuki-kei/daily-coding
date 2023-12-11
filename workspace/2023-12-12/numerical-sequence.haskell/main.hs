7de0268a27f3398eaf1c899f3eb9f75c00615451
SUZUKI Kei <todokimasen@gmail.com>
2023-12-12 08:52:11 +0900

    numerical-sequence.haskell

---
+++ workspace/2023-12-12/numerical-sequence.haskell/main.hs
@@ -0,0 +1,45 @@
+{-# LANGUAGE FlexibleInstances #-}
+{- cabal:
+    build-depends: base
+-}
+
+import Data.List
+import Text.Printf
+
+class ToString a where
+    toString :: a -> String
+
+instance ToString String where
+    toString s = s
+
+instance ToString Int where
+    toString n = printf "%d" n
+
+main :: IO ()
+main = do
+    printSequence "factorials" factorial [1..10]
+    printSequence "fibonacci numbers" fibonacci [0..20]
+    printSequence "fizz buzz values" fizzBuzz [1..100]
+
+printSequence :: (ToString a) => String -> (Int -> a) -> [Int] -> IO ()
+printSequence description f ns = do
+    let values = map f ns
+    let string = foldl (++) "" $ intersperse " " $ map toString values
+    printf "%s:\n%s\n" description string
+
+factorial :: Int -> Int
+factorial 0 = 1
+factorial n = n * factorial (n - 1)
+
+fibonacci :: Int -> Int
+fibonacci 0 = 0
+fibonacci 1 = 1
+fibonacci n = fibonacci (n - 1) + fibonacci (n - 2)
+
+fizzBuzz :: Int -> String
+fizzBuzz n
+    | n `mod` 15 == 0 = "FizzBuzz"
+    | n `mod`  5 == 0 = "Buzz"
+    | n `mod`  3 == 0 = "Fizz"
+    | otherwise       = printf "%d" n
+
