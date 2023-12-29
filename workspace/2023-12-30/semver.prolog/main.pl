a7dff9ba1fe99485edc092e628e7a323b206fa9e
SUZUKI Kei <todokimasen@gmail.com>
2023-12-30 08:10:11 +0900

    semver.prolog

---
+++ workspace/2023-12-30/semver.prolog/main.pl
@@ -0,0 +1,34 @@
+
+main :-
+    demonstration1,
+    demonstration2,
+    halt.
+
+demonstration1 :-
+    format("==== bump version~n"),
+    semver(Version, 1, 2, 3),
+    semver_bump_print(Version, semver_bump_major),
+    semver_bump_print(Version, semver_bump_minor),
+    semver_bump_print(Version, semver_bump_patch).
+
+demonstration2 :-
+    format("==== compare version~n"),
+    semver(Version, 1, 2, 3),
+    semver_bump_compare_print(Version, semver_bump_major),
+    semver_bump_compare_print(Version, semver_bump_minor),
+    semver_bump_compare_print(Version, semver_bump_patch).
+
+semver_bump_print(Version, BumpVersion) :-
+    call(BumpVersion, BumpedVersion, Version),
+    format("~w(~w, ~w).~n", [BumpVersion, BumpedVersion, Version]).
+
+semver_bump_compare_print(Version, BumpVersion) :-
+    call(BumpVersion, BumpedVersion, Version),
+    semver_compare_print(BumpedVersion, Version),
+    semver_compare_print(Version, BumpedVersion),
+    semver_compare_print(BumpedVersion, BumpedVersion).
+
+semver_compare_print(Version1, Version2) :-
+    semver_compare(Compared, Version1, Version2),
+    format("semver_compare(~w, ~w, ~w).~n", [Compared, Version1, Version2]).
+
