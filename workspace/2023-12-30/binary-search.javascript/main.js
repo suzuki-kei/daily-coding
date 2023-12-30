73bf81b1bf76111cfd853c5fecea89090d81b6ee
SUZUKI Kei <todokimasen@gmail.com>
2023-12-30 09:10:53 +0900

    binary-search.javascript

---
+++ workspace/2023-12-30/binary-search.javascript/main.js
@@ -0,0 +1,45 @@
+
+function main()
+{
+    const values = generate_random_ascending_values(20)
+    console.log(values.join(" "))
+
+    const lower = values.at(0) - 1
+    const upper = values.at(-1) + 1
+
+    for (let target = lower; target <= upper; target++)
+    {
+        const index = binary_search(values, target)
+        console.log(`target=${target}, index=${index}`)
+    }
+}
+
+function generate_random_ascending_values(n)
+{
+    return Array.from(Array(n)).reduce((values) => {
+        const offset = (values.length === 0 ? 9 : values.at(-1)) + 1
+        values.push(Math.floor(Math.random() * 5 + offset))
+        return values
+    }, [])
+}
+
+function binary_search(values, target)
+{
+    return binary_search_range(values, target, 0, values.length - 1)
+}
+
+function binary_search_range(values, target, lower, upper)
+{
+    const center = Math.floor((lower + upper) / 2)
+
+    if (center < lower || upper < center)
+        return -1
+    if (target < values[center])
+        return binary_search_range(values, target, lower, center - 1)
+    if (target > values[center])
+        return binary_search_range(values, target, center + 1, upper)
+    return center
+}
+
+main()
+
