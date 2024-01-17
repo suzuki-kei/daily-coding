09a59c5067871348a4a052a7388a4608debb9cc9
SUZUKI Kei <todokimasen@gmail.com>
2024-01-17 12:25:03 +0900

    fizz-buzz.python

---
+++ workspace/2024-01-17/fizz-buzz.python/main.py
@@ -0,0 +1,30 @@
+import itertools
+
+
+def main():
+    for value in generate_fizz_buzz_values(100):
+        print(value)
+
+
+def generate_fizz_buzz_values(n=None):
+    tuples = zip(
+        itertools.cycle([None] * 14 + ["FizzBuzz"]),
+        itertools.cycle([None] *  4 + ["Buzz"]),
+        itertools.cycle([None] *  2 + ["Fizz"]),
+        itertools.count(1, 1))
+    values = map(next_not_none, tuples)
+
+    if n is None:
+        yield from values
+    else:
+        yield from itertools.islice(values, n)
+
+
+def next_not_none(iterable):
+    is_none = lambda x: x is None
+    return next(itertools.dropwhile(is_none, iterable))
+
+
+if __name__ == "__main__":
+    main()
+
