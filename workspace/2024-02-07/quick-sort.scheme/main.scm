f87e5b8276b507b3d719f5b4e83654e3872d5ff8
SUZUKI Kei <todokimasen@gmail.com>
2024-02-07 09:52:14 +0900

    quick-sort.scheme

---
+++ workspace/2024-02-07/quick-sort.scheme/main.scm
@@ -0,0 +1,122 @@
+(use srfi-13)
+(use srfi-27)
+
+(define main
+    (lambda (_)
+        (initialize)
+        (demonstration)
+        0))
+
+(define initialize
+    (lambda ()
+        (random-source-randomize! default-random-source)))
+
+(define demonstration
+    (lambda ()
+        (let ((xs (generate-random-values 20)))
+            (print-values xs)
+            (print-values (quick-sort xs)))))
+
+(define generate-random-values
+    (lambda (n)
+        (map
+            (lambda (_)
+                (+
+                    (random-integer 90)
+                    10))
+            (iota n))))
+
+(define print-values
+    (lambda (xs)
+        (print
+            (format
+                (if (sorted? xs)
+                    "~a (sorted)"
+                    "~a (not sorted)")
+                (xs->string " " xs)))))
+
+(define sorted?
+    (lambda (xs)
+        (cond
+            ((null? xs)
+                #t)
+            ((null? (cdr xs))
+                #t)
+            (else
+                (and
+                    (<= (car xs) (cadr xs))
+                    (sorted? (cdr xs)))))))
+
+(define xs->string
+    (lambda (separator xs)
+        (string-concatenate
+            (map
+                x->string
+                (intersperse separator xs)))))
+
+(define intersperse
+    (lambda (separator xs)
+        (cond
+            ((null? xs)
+                '())
+            (else
+                (intersperse.tailrec '() separator xs)))))
+
+(define intersperse.tailrec
+    (lambda (accumulated separator xs)
+        (cond
+            ((null? xs)
+                (reverse accumulated))
+            ((null? accumulated)
+                (intersperse.tailrec
+                    (list (car xs))
+                    separator
+                    (cdr xs)))
+            (else
+                (intersperse.tailrec
+                    (cons*
+                        (car xs)
+                        separator
+                        accumulated)
+                    separator
+                    (cdr xs))))))
+
+(define quick-sort
+    (lambda (xs)
+        (cond
+            ((null? xs)
+                '())
+            (else
+                (receive
+                    (less-xs equal-xs greater-xs)
+                    (partition (car xs) xs)
+                    (append
+                        (quick-sort less-xs)
+                        equal-xs
+                        (quick-sort greater-xs)))))))
+
+(define partition
+    (lambda (pivot xs)
+        (partition.tailrec '() '() '() pivot xs)))
+
+(define partition.tailrec
+    (lambda (less-xs equal-xs greater-xs pivot xs)
+        (cond
+            ((null? xs)
+                (values less-xs equal-xs greater-xs))
+            (else
+                (partition.tailrec
+                    (cons-if < (car xs) pivot less-xs)
+                    (cons-if = (car xs) pivot equal-xs)
+                    (cons-if > (car xs) pivot greater-xs)
+                    pivot
+                    (cdr xs))))))
+
+(define cons-if
+    (lambda (predicate x pivot xs)
+        (cond
+            ((predicate x pivot)
+                (cons x xs))
+            (else
+                xs))))
+
