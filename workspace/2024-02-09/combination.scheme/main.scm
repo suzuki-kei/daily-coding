50b56324160d400f8b9315a34975610a5d9ecc85
SUZUKI Kei <todokimasen@gmail.com>
2024-02-09 14:31:00 +0900

    combination.scheme

---
+++ workspace/2024-02-09/combination.scheme/main.scm
@@ -0,0 +1,43 @@
+
+(define main
+    (lambda (_)
+        (demonstration)
+        0))
+
+(define demonstration
+    (lambda ()
+        (for-each
+            (lambda (n)
+                (for-each
+                    (lambda (r)
+                        (print-combination n r))
+                    (iota (+ n 1))))
+            (iota 7))))
+
+(define print-combination
+    (lambda (n r)
+        (let ((c (combination n r)))
+            (print
+                (format "C(n=~d, r=~d) = ~d" n r c)))))
+
+; C(n, r)   = n! / (r! * (n-r)!)
+; C(n, r-1) = n! / ((r-1)! * (n-(r-1))!)
+; r!        = r * (r-1)!
+; (n-r)!    = (n-(r-1))! / (n-(r-1))
+; C(n, r)   = C(n, r-1) * (n-(r-1)) / r
+(define combination
+    (lambda (n r)
+        (cond
+            ((or (= n 0) (= r 0) (= n r))
+                1)
+            (else
+                (/
+                    (*
+                        (combination
+                            n
+                            (- r 1))
+                        (-
+                            n
+                            (- r 1)))
+                    r)))))
+
