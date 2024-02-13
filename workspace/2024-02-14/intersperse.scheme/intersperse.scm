ef3bb03a860cee3e2cfb38851e87fb48af9a9c17
SUZUKI Kei <todokimasen@gmail.com>
2024-02-14 08:56:13 +0900

    intersperse.scheme

---
+++ workspace/2024-02-14/intersperse.scheme/intersperse.scm
@@ -0,0 +1,24 @@
+
+(define intersperse
+    (lambda (separator xs)
+        (cond
+            ((null? xs)
+                '())
+            (else
+                (intersperse.tailrec '() separator xs)))))
+
+(define intersperse.tailrec
+    (lambda (accumulated separator xs)
+        (cond
+            ((null? xs)
+                (cdr
+                    (reverse accumulated)))
+            (else
+                (intersperse.tailrec
+                    (cons*
+                        (car xs)
+                        separator
+                        accumulated)
+                    separator
+                    (cdr xs))))))
+
