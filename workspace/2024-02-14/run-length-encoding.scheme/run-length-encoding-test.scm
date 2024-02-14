17fb636c13534097dd3eec8ba5ad63b9d3f23956
SUZUKI Kei <todokimasen@gmail.com>
2024-02-14 10:16:22 +0900

    run-length-encoding.scheme

---
+++ workspace/2024-02-14/run-length-encoding.scheme/run-length-encoding-test.scm
@@ -0,0 +1,51 @@
+(use gauche.test)
+(load "run-length-encoding")
+
+(define main
+    (lambda (_)
+        (test-start "run-length-encoding")
+        (test-encode)
+        (test-decode)
+        (test-end)))
+
+(define TEST-DATA-TUPLES '(
+    ; (xs encoded-runs)
+    (()            ())
+    ((a)           ((a . 1)))
+    ((a a)         ((a . 2)))
+    ((a a a)       ((a . 3)))
+    ((a b)         ((a . 1) (b . 1)))
+    ((a b b)       ((a . 1) (b . 2)))
+    ((a a b)       ((a . 2) (b . 1)))
+    ((a a b c c c) ((a . 2) (b . 1) (c . 3)))))
+
+(define for-each-test-data
+    (lambda (callback)
+        (for-each
+            (lambda (indexed-tuple)
+                (apply callback indexed-tuple))
+            (map
+                cons
+                (iota (length TEST-DATA-TUPLES))
+                TEST-DATA-TUPLES))))
+
+(define test-encode
+    (lambda ()
+        (test-section "encode")
+        (for-each-test-data
+            (lambda (index xs encoded-runs)
+                (test*
+                    (format "#~d" (+ index 1))
+                    encoded-runs
+                    (encode xs))))))
+
+(define test-decode
+    (lambda ()
+        (test-section "decode")
+        (for-each-test-data
+            (lambda (index xs encoded-runs)
+                (test*
+                    (format "#~d" (+ index 1))
+                    xs
+                    (decode encoded-runs))))))
+
