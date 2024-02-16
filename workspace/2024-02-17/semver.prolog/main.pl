e00423ed0ac10f22a48efe6df1612d692ebba637
SUZUKI Kei <todokimasen@gmail.com>
2024-02-17 06:01:54 +0900

    semver.prolog

---
+++ workspace/2024-02-17/semver.prolog/main.pl
@@ -0,0 +1,43 @@
+
+main :-
+    demonstration1,
+    demonstration2,
+    halt.
+
+commit(incompatible_changed, "commit 1").
+commit(incompatible_changed, "commit 2").
+commit(compatible_added, "commit 3").
+commit(compatible_added, "commit 4").
+commit(compatible_fixed, "commit 5").
+commit(compatible_fixed, "commit 6").
+
+demonstration1 :-
+    format("==== bump version~n"),
+    semver(Version, 1, 2, 3),
+    findall(commit(Type, Message), commit(Type, Message), Commits),
+    semver_print_changes(Version, Commits).
+
+demonstration2 :-
+    format("==== compare version~n"),
+    semver(Version, 1, 2, 3),
+    semver_bump_compare_print(Version, semver_bump_major),
+    semver_bump_compare_print(Version, semver_bump_minor),
+    semver_bump_compare_print(Version, semver_bump_patch).
+
+semver_print_changes(_, []).
+
+semver_print_changes(Version, [Commit|Commits]) :-
+    semver_bump_version(BumpedVersion, Version, Commit),
+    format("semver_bump_version(~w, ~w, ~w).~n", [BumpedVersion, Version, Commit]),
+    semver_print_changes(BumpedVersion, Commits).
+
+semver_bump_compare_print(Version, BumpVersion) :-
+    call(BumpVersion, BumpedVersion, Version),
+    semver_compare_print(BumpedVersion, BumpedVersion),
+    semver_compare_print(BumpedVersion, Version),
+    semver_compare_print(Version, BumpedVersion).
+
+semver_compare_print(Version1, Version2) :-
+    semver_compare(Compared, Version1, Version2),
+    format("semver_compare(~w, ~w, ~w).~n", [Compared, Version1, Version2]).
+
