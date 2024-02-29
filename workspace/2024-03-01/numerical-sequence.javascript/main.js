c5ea2fb4399400eb3d8ee19ecb86a23927bb14be
SUZUKI Kei <todokimasen@gmail.com>
2024-03-01 07:01:32 +0900

    numerical-sequence.javascript

---
+++ workspace/2024-03-01/numerical-sequence.javascript/main.js
@@ -0,0 +1,56 @@
+
+function main()
+{
+    print_sequence("factorials", factorial, 1, 10)
+    print_sequence("fibonacci numbers", fibonacci, 0, 20)
+    print_sequence("fizz buzz values", fizz_buzz, 1, 100)
+}
+
+function print_sequence(description, f, min, max)
+{
+    console.log(`${description}:`)
+    console.log(sequence(min, max).map(f).join(" "))
+}
+
+function sequence(min, max)
+{
+    return [...Array(max - min + 1).keys()]
+}
+
+function factorial(n)
+{
+    return factorial_tailrec(n, 1)
+}
+
+function factorial_tailrec(n, fn)
+{
+    if (n === 0)
+        return fn
+    return factorial_tailrec(n - 1, fn * n)
+}
+
+function fibonacci(n)
+{
+    return fibonacci_tailrec(n, 0, 1)
+}
+
+function fibonacci_tailrec(n, a, b)
+{
+    if (n === 0)
+        return a
+    return fibonacci_tailrec(n - 1, b, a + b)
+}
+
+function fizz_buzz(n)
+{
+    if (n % 15 === 0)
+        return "FizzBuzz"
+    if (n % 5 === 0)
+        return "Buzz"
+    if (n % 3 === 0)
+        return "Fizz"
+    return String(n)
+}
+
+main()
+
