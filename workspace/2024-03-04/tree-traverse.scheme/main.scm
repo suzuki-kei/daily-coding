f36374b0a18dfda4a1f01bac64d7118ed3e58098
SUZUKI Kei <todokimasen@gmail.com>
2024-03-04 19:35:11 +0900

    tree-traverse.scheme

---
+++ workspace/2024-03-04/tree-traverse.scheme/main.scm
@@ -0,0 +1,47 @@
+
+(define main
+    (lambda (_)
+        (demonstration '(a (b) ((c d) e)))
+        (demonstration '((a (b c)) (d) e))
+        0))
+
+(define-syntax trace
+    (syntax-rules ()
+        ((_ x)
+            (begin
+                (print "==== " 'x)
+                (print x)))))
+
+(define demonstration
+    (lambda (tree)
+        (print (make-string 80 #\-))
+        (trace tree)
+        (trace (traverse for-each print atom? tree))
+        (trace (traverse (reversed for-each) print atom? tree))
+        (trace (traverse map values atom? tree))
+        (trace (traverse (reversed map) values atom? tree))))
+
+(define traverse
+    (lambda (process-nodes process-leaf leaf? tree)
+        (define process-node
+            (lambda (node)
+                (cond
+                    ((leaf? node)
+                        (process-leaf node))
+                    (else
+                        (traverse process-nodes process-leaf leaf? node)))))
+        (process-nodes process-node tree)))
+
+(define atom?
+    (lambda (x)
+        (and
+            (not (null? x))
+            (not (pair? x)))))
+
+(define reversed
+    (lambda (process-nodes)
+        (lambda (process-node nodes)
+            (process-nodes
+                process-node
+                (reverse nodes)))))
+
