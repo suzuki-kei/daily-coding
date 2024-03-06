19a067f31afb5908f9cb738ef8c5a611bf3cc90a
SUZUKI Kei <todokimasen@gmail.com>
2024-03-07 08:00:43 +0900

    flatten.scheme

---
+++ workspace/2024-03-07/flatten.scheme/flatten-test.scm
@@ -0,0 +1,26 @@
+(use gauche.test)
+(load "flatten")
+
+(define main
+    (lambda (_)
+        (test-start "flatten")
+        (test-flatten)
+        (test-end)))
+
+(define test-flatten
+    (lambda ()
+        (test-section "flatten")
+        (test* "#1" '() (flatten '()))
+        (test* "#2" '() (flatten '(())))
+        (test* "#3" '() (flatten '((()))))
+        (test* "#4" '() (flatten '(() ())))
+        (test* "#5" '(a) (flatten '(a)))
+        (test* "#6" '(a) (flatten '((a))))
+        (test* "#7" '(a) (flatten '(((a)))))
+        (test* "#8" '(a b) (flatten '(a b)))
+        (test* "#9" '(a b) (flatten '(a (b))))
+        (test* "#10" '(a b) (flatten '((a) b)))
+        (test* "#11" '(a b) (flatten '((a) (b))))
+        (test* "#12" '(a b c d e) (flatten '(a (b) ((c d) e))))
+        (test* "#13" '(a b c d e) (flatten '((a (b c)) (d) e)))))
+
