e8ffcf6befc289f23fe8c4470dcc85cdc222edda
SUZUKI Kei <todokimasen@gmail.com>
2024-03-23 10:58:40 +0900

    lps.ruby

---
+++ workspace/2024-03-23/lps.ruby/lps.rb
@@ -0,0 +1,43 @@
+
+module Lps
+
+    def prefixes
+        (0..size).map do |n|
+            self[...n]
+        end
+    end
+
+    def proper_prefixes
+        (0...size).map do |n|
+            self[...n]
+        end
+    end
+
+    def suffixes
+        (0..size).map do |n|
+            self[size-n...size]
+        end
+    end
+
+    def proper_suffixes
+        (0...size).map do |n|
+            self[size-n...size]
+        end
+    end
+
+    def lps
+        (proper_prefixes & proper_suffixes).max_by(&:size)
+    end
+
+    def lps_array
+        prefixes[1..].map(&:lps).map(&:size)
+    end
+
+end
+
+class String
+
+    include Lps
+
+end
+
