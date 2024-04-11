61b45a226dfbeec9d72ea70a29d6819922c2d8f1
SUZUKI Kei <todokimasen@gmail.com>
2024-04-12 08:03:54 +0900

    intersperse.scheme

---
+++ workspace/2024-04-12/intersperse.scheme/intersperse-test.scm
@@ -0,0 +1,18 @@
+(use gauche.test)
+(load "intersperse")
+
+(define main
+    (lambda (_)
+        (test-start "intersperse")
+        (test-intersperse)
+        (test-end)))
+
+(define test-intersperse
+    (lambda ()
+        (test-section "intersperse")
+        (test* "#1" '() (intersperse 'x '()))
+        (test* "#2" '(a) (intersperse 'x '(a)))
+        (test* "#3" '(a x b) (intersperse 'x '(a b)))
+        (test* "#4" '(a x b x c) (intersperse 'x '(a b c)))
+        (test* "#5" '(a _ b _ c) (intersperse '_ '(a b c)))))
+
