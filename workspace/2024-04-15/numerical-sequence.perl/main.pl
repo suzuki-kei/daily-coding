eb5a6ade4e9be4a9abe8d3f08e1ebd3e01fb115e
SUZUKI Kei <todokimasen@gmail.com>
2024-04-15 09:50:52 +0900

    numerical-sequence.perl

---
+++ workspace/2024-04-15/numerical-sequence.perl/main.pl
@@ -0,0 +1,58 @@
+use strict;
+use warnings;
+
+sub main
+{
+    print_sequence('factorials', \&factorial, 1, 10);
+    print_sequence('fibonacci numbers', \&fibonacci, 0, 20);
+    print_sequence('fizz buzz values', \&fizz_buzz, 1, 100);
+}
+
+sub print_sequence
+{
+    my ($description, $f, $min, $max) = @_;
+    my @values = map { $f->($_) } $min..$max;
+    my $string = join ' ', @values;
+
+    print "${description}:\n";
+    print "${string}\n";
+}
+
+sub factorial
+{
+    return factorial_tailrec(@_, 1);
+}
+
+sub factorial_tailrec
+{
+    my ($n, $accumulated) = @_;
+
+    return $accumulated if $n == 0;
+    return factorial_tailrec($n - 1, $accumulated * $n);
+}
+
+sub fibonacci
+{
+    return fibonacci_tailrec(@_, 0, 1);
+}
+
+sub fibonacci_tailrec
+{
+    my ($n, $a, $b) = @_;
+
+    return $a if $n == 0;
+    return fibonacci_tailrec($n - 1, $b, $a + $b);
+}
+
+sub fizz_buzz
+{
+    my $n = shift;
+
+    return 'FizzBuzz' if $n % 15 == 0;
+    return 'Buzz' if $n % 5 == 0;
+    return 'Fizz' if $n % 3 == 0;
+    return $n;
+}
+
+main;
+
