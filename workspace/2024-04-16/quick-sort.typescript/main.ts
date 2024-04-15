b3c3c16dfdf878d636e4b81291f7829fdc0dc250
SUZUKI Kei <todokimasen@gmail.com>
2024-04-16 07:32:49 +0900

    quick-sort.typescript

---
+++ workspace/2024-04-16/quick-sort.typescript/main.ts
@@ -0,0 +1,69 @@
+
+function main(): void
+{
+    const values = generate_random_values(20)
+    print_array(values)
+    quick_sort(values)
+    print_array(values)
+}
+
+function generate_random_values(n: number): number[]
+{
+    const values = []
+
+    for (let i = 0; i < n; i++)
+        values.push(Math.floor(Math.random() * 90 + 10))
+
+    return values
+}
+
+function print_array<T>(values: T[]): void
+{
+    if (is_sorted(values))
+        console.log(`${values.join(" ")} (sorted)`)
+    else
+        console.log(`${values.join(" ")} (not sorted)`)
+}
+
+function is_sorted<T>(values: T[]): boolean
+{
+    for (let i = 0; i < values.length - 1; i++)
+        if (values[i] > values[i + 1])
+            return false
+    return true
+}
+
+function quick_sort<T>(values: T[]): void
+{
+    quick_sort_range(values, 0, values.length - 1)
+}
+
+function quick_sort_range<T>(values: T[], lower: number, upper: number): void
+{
+    let lower_index = lower
+    let upper_index = upper
+    const pivot = values[Math.floor((lower + upper) / 2)]
+
+    while (lower_index <= upper_index)
+    {
+        while (values[lower_index] < pivot)
+            lower_index++
+        while (values[upper_index] > pivot)
+            upper_index--
+        if (lower_index <= upper_index)
+            swap(values, lower_index++, upper_index--)
+    }
+
+    if (lower < upper_index)
+        quick_sort_range(values, lower, upper_index)
+    if (lower_index < upper)
+        quick_sort_range(values, lower_index, upper)
+}
+
+function swap<T>(values: T[], index1: number, index2: number): void
+{
+    [values[index1], values[index2]] = [values[index2], values[index1]]
+}
+
+main()
+
