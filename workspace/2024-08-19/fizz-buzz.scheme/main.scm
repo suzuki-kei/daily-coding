d6808cc0c2da902bb1338bf54187170a5c1b89c0
SUZUKI Kei <todokimasen@gmail.com>
2024-08-19 00:07:24 +0900

    fizz-buzz.scheme

---
+++ workspace/2024-08-19/fizz-buzz.scheme/main.scm
@@ -0,0 +1,26 @@
+
+(define main
+    (lambda (_)
+        (for-each
+            print
+            (fizz-buzz-values 100))
+        0))
+
+(define fizz-buzz
+    (lambda (n)
+        (cond
+            ((= (modulo n 15) 0)
+                'FizzBuzz)
+            ((= (modulo n 5) 0)
+                'Buzz)
+            ((= (modulo n 3) 0)
+                'Fizz)
+            (else
+                n))))
+
+(define fizz-buzz-values
+    (lambda (n)
+        (map
+            fizz-buzz
+            (iota n 1))))
+
