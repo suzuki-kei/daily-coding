861df0f98cd87e8c37f9c444eb416128b33beff7
SUZUKI Kei <todokimasen@gmail.com>
2024-09-22 10:56:59 +0900

    fizz-buzz.csharp

---
+++ workspace/2024-09-22/fizz-buzz.csharp/main.cs
@@ -0,0 +1,23 @@
+using System;
+
+class App
+{
+
+    public static void Main()
+    {
+        for (int i = 1; i <= 100; i++)
+            System.Console.WriteLine(FizzBuzz(i));
+    }
+
+    private static string FizzBuzz(int n)
+    {
+        return (n % 5, n % 3) switch {
+            (0, 0) => "FizzBuzz",
+            (0, _) => "Buzz",
+            (_, 0) => "Fizz",
+            (_, _) => n.ToString(),
+        };
+    }
+
+}
+
