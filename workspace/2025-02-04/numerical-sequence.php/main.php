1a6d5a9087b724c3bab834503575d93e53753472
SUZUKI Kei <todokimasen@gmail.com>
2025-02-04 21:09:29 +0900

    numerical-sequence.php

---
+++ workspace/2025-02-04/numerical-sequence.php/main.php
@@ -0,0 +1,61 @@
+<?php
+
+function main(): void
+{
+    print_sequence('factorials', 'factorial', 1, 10);
+    print_sequence('fibonacci numbers', 'fibonacci', 0, 20);
+    print_sequence('fizz buzz values', 'fizz_buzz', 1, 100);
+}
+
+function print_sequence(string $description, callable $f, int $min, int $max): void
+{
+    $values = array_map($f, range($min, $max));
+    $string = implode(' ', $values);
+
+    echo "{$description}:\n";
+    echo "{$string}\n";
+}
+
+function factorial(int $n): int
+{
+    return factorial_tailrec($n, 1);
+}
+
+function factorial_tailrec(int $n, int $accumulated): int
+{
+    return match($n)
+    {
+        0       => $accumulated,
+        default => factorial_tailrec($n - 1, $accumulated * $n),
+    };
+}
+
+function fibonacci(int $n): int
+{
+    return fibonacci_tailrec($n, 0, 1);
+}
+
+function fibonacci_tailrec(int $n, int $a, int $b): int
+{
+    return match($n)
+    {
+        0       => $a,
+        default => fibonacci_tailrec($n - 1, $b, $a + $b),
+    };
+}
+
+function fizz_buzz(int $n): string
+{
+    return match(($n % 5 == 0 ? 0b10 : 0b00) + ($n % 3 == 0 ? 0b01 : 0b00))
+    {
+        0b11 => 'FizzBuzz',
+        0b10 => 'Buzz',
+        0b01 => 'Fizz',
+        0b00 => strval($n),
+    };
+}
+
+main();
+
+?>
+
