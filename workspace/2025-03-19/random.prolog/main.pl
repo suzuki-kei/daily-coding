4360ccad9e70fe0572d3be5774a83494b15de269
SUZUKI Kei <todokimasen@gmail.com>
2025-03-19 05:52:27 +0900

    random.prolog

---
+++ workspace/2025-03-19/random.prolog/main.pl
@@ -0,0 +1,16 @@
+
+main :-
+    random_values(Xs, 20),
+    writeln(Xs),
+    halt.
+
+random_values(Xs, N) :-
+    random_values(Xs, [], N).
+
+random_values(Xs, Xs, 0).
+
+random_values(Xs, Accumulated, N) :-
+    is(NextN, N - 1),
+    random_between(10, 99, X),
+    random_values(Xs, [X|Accumulated], NextN).
+
