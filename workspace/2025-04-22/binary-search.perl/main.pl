5b7bfe8e5997afd401d9cf0969260cdd7c0a5044
SUZUKI Kei <todokimasen@gmail.com>
2025-04-22 07:48:43 +0900

    binary-search.perl

---
+++ workspace/2025-04-22/binary-search.perl/main.pl
@@ -0,0 +1,50 @@
+use strict;
+use warnings;
+
+sub main
+{
+    my @values = generate_random_ascending_values(20);
+    print join(' ', @values), "\n";
+
+    my $lower = $values[0] - 1;
+    my $upper = $values[-1] + 1;
+
+    for my $target ($lower .. $upper) {
+        my $index = binary_search(\@values, $target);
+        print "target=${target}, index=${index}\n";
+    }
+}
+
+sub generate_random_ascending_values
+{
+    my ($n) = @_;
+    my @values = ();
+
+    for my $i (1 .. $n) {
+        my $offset = ($values[-1] || 9) + 1;
+        push(@values, int(rand(5)) + $offset);
+    }
+
+    return @values;
+}
+
+sub binary_search
+{
+    my ($values_ref, $target, $lower, $upper) = @_;
+    $lower //= 0;
+    $upper //= (scalar @$values_ref) - 1;
+
+    return -1 if $lower > $upper;
+
+    my $center = int(($lower + $upper) / 2);
+    return $center if $target == @$values_ref[$center];
+
+    if ($target < @$values_ref[$center]) {
+        return binary_search($values_ref, $target, $lower, $center - 1);
+    } else {
+        return binary_search($values_ref, $target, $center + 1, $upper);
+    }
+}
+
+main();
+
