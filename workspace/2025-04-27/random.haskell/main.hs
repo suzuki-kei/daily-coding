82b238b15deb9589bb56bce90d2e380e72859637
SUZUKI Kei <todokimasen@gmail.com>
2025-04-27 08:47:01 +0900

    random.haskell

---
+++ workspace/2025-04-27/random.haskell/main.hs
@@ -0,0 +1,29 @@
+import Data.List (intersperse)
+import System.Random (StdGen)
+import System.Random (getStdGen)
+import System.Random (randomR)
+import Text.Printf (printf)
+
+main :: IO ()
+main = do
+    gen <- getStdGen
+    let (xs, gen') = generateRandomValues 20 gen
+    printf "%s\n" $ listToString " " xs
+
+toString :: Int -> String
+toString = printf "%d"
+
+listToString :: String -> [Int] -> String
+listToString separator xs = foldl (++) "" $ intersperse separator $ map toString xs
+
+generateRandomValues :: Int -> StdGen -> ([Int], StdGen)
+generateRandomValues n gen = accumulate [] n gen
+    where
+        accumulate xs 0 gen =
+            (xs, gen)
+        accumulate xs n gen =
+            let
+                (x, gen') = randomR (10, 99) gen
+            in
+                accumulate (x:xs) (n - 1) gen'
+
