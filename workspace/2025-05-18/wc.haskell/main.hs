f36a483634896969876ace72d1e9a6968b6b8076
SUZUKI Kei <todokimasen@gmail.com>
2025-05-18 10:46:25 +0900

    wc.haskell

---
+++ workspace/2025-05-18/wc.haskell/main.hs
@@ -0,0 +1,20 @@
+import Data.Char (isSpace)
+
+main :: IO ()
+main = getContents >>= print . wc
+
+wc :: String -> (Int, Int, Int)
+wc s = accumulate s False 0 0 0
+    where
+        accumulate "" inWord lineCount wordCount charCount =
+            (lineCount, wordCount, charCount)
+        accumulate (c : chars) inWord lineCount wordCount charCount
+            | isNewLine c = accumulate chars False (lineCount + 1) wordCount (charCount + 1)
+            | isSpace   c = accumulate chars False lineCount wordCount (charCount + 1)
+            | otherwise   =
+                if inWord then
+                    accumulate chars True lineCount wordCount (charCount + 1)
+                else
+                    accumulate chars True lineCount (wordCount + 1) (charCount + 1)
+        isNewLine c = c == '\n'
+
