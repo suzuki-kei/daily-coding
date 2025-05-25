74abd26a7750b895efb2ccd5a4d98b1ba18c7623
SUZUKI Kei <todokimasen@gmail.com>
2025-05-25 09:27:25 +0900

    wc.haskell

--- workspace/2025-05-25/wc.haskell/main.hs
+++ workspace/2025-05-25/wc.haskell/main.hs
@@ -8,17 +8,16 @@ type WordCount = Int
 type CharCount = Int
 
 wc :: String -> (LineCount, WordCount, CharCount)
-wc s = accumulate s False 0 0 0
+wc s =
+    let
+        (inWord, lineCount, wordCount, charCount) = foldl folder (False, 0, 0, 0) s
+    in
+        (lineCount, wordCount, charCount)
     where
-        accumulate "" inWord lineCount wordCount charCount =
-            (lineCount, wordCount, charCount)
-        accumulate (c : chars) inWord lineCount wordCount charCount
-            | isNewLine c = accumulate chars False (lineCount + 1) wordCount (charCount + 1)
-            | isSpace c   = accumulate chars False lineCount wordCount (charCount + 1)
-            | otherwise   =
-                if inWord then
-                    accumulate chars True lineCount wordCount (charCount + 1)
-                else
-                    accumulate chars True lineCount (wordCount + 1) (charCount + 1)
+        folder (inWord, lineCount, wordCount, charCount) c
+            | isNewLine c = (False, lineCount + 1, wordCount, charCount + 1)
+            | isSpace c   = (False, lineCount, wordCount, charCount + 1)
+            | inWord      = (True, lineCount, wordCount, charCount + 1)
+            | otherwise   = (True, lineCount, wordCount + 1, charCount + 1)
         isNewLine c = c == '\n'
 
