088f26f888ff3bbd0d048c86d5e3517821f7a005
SUZUKI Kei <todokimasen@gmail.com>
2025-05-28 20:59:38 +0900

    wc.haskell

---
+++ workspace/2025-05-28/wc.haskell/main.hs
@@ -0,0 +1,26 @@
+import Data.Char (isSpace)
+
+main :: IO ()
+main = getContents >>= print . wc
+
+type LineCount = Int
+type WordCount = Int
+type CharCount = Int
+
+wc :: String -> (LineCount, WordCount, CharCount)
+wc s =
+    let
+        (inWord, lineCount, wordCount, charCount) = foldl folder (False, 0, 0, 0) s
+    in
+        (lineCount, wordCount, charCount)
+    where
+        folder (inWord, lineCount, wordCount, charCount) c
+            | isNewLine c = (False, lineCount + 1, wordCount, charCount + 1)
+            | isSpace c   = (False, lineCount, wordCount, charCount + 1)
+            | otherwise   =
+                if inWord then
+                    (True, lineCount, wordCount, charCount + 1)
+                else
+                    (True, lineCount, wordCount + 1, charCount + 1)
+        isNewLine c = c == '\n'
+
