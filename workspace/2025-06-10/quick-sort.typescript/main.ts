791ba85975d4b6ed1662e7321f1bb568d433290d
SUZUKI Kei <todokimasen@gmail.com>
2025-06-10 22:31:47 +0900

    quick-sort.typescript

---
+++ workspace/2025-06-10/quick-sort.typescript/main.ts
@@ -0,0 +1,67 @@
+
+function main(): void
+{
+    const values = generate_random_values(20)
+
+    print_array(values)
+    quick_sort(values)
+    print_array(values)
+}
+
+function generate_random_values(n: number): number[]
+{
+    return [...Array(n)].map(_ => Math.floor(Math.random() * 90) + 10)
+}
+
+function print_array(values: number[]): void
+{
+    if (is_sorted(values))
+        console.log(`${values.join(" ")} (sorted)`)
+    else
+        console.log(`${values.join(" ")} (not sorted)`)
+}
+
+function is_sorted(values: number[]): boolean
+{
+    return [...Array(values.length - 1).keys()].every(i => values[i] <= values[i + 1])
+}
+
+function quick_sort(values: number[]): void
+{
+    quick_sort_range(values, 0, values.length - 1)
+}
+
+function quick_sort_range(values: number[], lower: number, upper: number): void
+{
+    let increment_index = lower
+    let decrement_index = upper
+    const pivot = random_select(values, lower, upper)
+
+    while (increment_index <= decrement_index)
+    {
+        while (values[increment_index] < pivot)
+            increment_index++
+        while (values[decrement_index] > pivot)
+            decrement_index--
+        if (increment_index <= decrement_index)
+            swap(values, increment_index++, decrement_index--)
+    }
+
+    if (lower < decrement_index)
+        quick_sort_range(values, lower, decrement_index)
+    if (increment_index < upper)
+        quick_sort_range(values, increment_index, upper)
+}
+
+function random_select(values: number[], lower: number, upper: number): number
+{
+    return values[Math.floor(Math.random() * (upper - lower + 1)) + lower]
+}
+
+function swap(values: number[], index1: number, index2: number): void
+{
+    [values[index1], values[index2]] = [values[index2], values[index1]]
+}
+
+main()
+
