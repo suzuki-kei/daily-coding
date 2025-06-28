58c6ad4b60049ace8fd14867039c62191a68c630
SUZUKI Kei <todokimasen@gmail.com>
2025-06-28 15:16:46 +0900

    fizz-buzz.python

---
+++ workspace/2025-06-28/fizz-buzz.python/main.py
@@ -0,0 +1,30 @@
+from typing import Any
+from typing import Iterable
+from typing import Iterator
+from typing import Optional
+import itertools
+
+
+def main() -> None:
+    print(" ".join(generate_fizz_buzz_values(100)))
+
+
+def generate_fizz_buzz_values(n: Optional[int] = None) -> Iterator[str]:
+    tuples = zip(
+        itertools.cycle([None] * 14 + ["FizzBuzz"]),
+        itertools.cycle([None] *  4 + ["Buzz"]),
+        itertools.cycle([None] *  2 + ["Fizz"]),
+        map(str, itertools.count(1, 1)))
+    values = map(first_not_none, tuples)
+    yield from itertools.islice(values, n)
+
+
+def first_not_none(iterable: Iterable[Any]) -> Any:
+    is_none = lambda x: x is None
+    dropped = itertools.dropwhile(is_none, iterable)
+    return next(dropped)
+
+
+if __name__ == "__main__":
+    main()
+
