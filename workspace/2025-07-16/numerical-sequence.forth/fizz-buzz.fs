028f5cc9ad1080cf39f6bb7b9511b8a56f69098d
SUZUKI Kei <todokimasen@gmail.com>
2025-07-16 07:35:29 +0900

    numerical-sequence.forth

---
+++ workspace/2025-07-16/numerical-sequence.forth/fizz-buzz.fs
@@ -0,0 +1,33 @@
+
+: fizz_buzz ( n -- c-addr u )
+    dup 15 mod 0 = if
+        drop s" FizzBuzz"
+    else
+        dup 5 mod 0 = if
+            drop s" Buzz"
+        else
+            dup 3 mod 0 = if
+                drop s" Fizz"
+            else
+                s>d <# #s #>
+            endif
+        endif
+    endif
+;
+
+: print_fizz_buzz_values_range recursive ( max min -- )
+    over over < if
+        drop drop
+    else
+        dup fizz_buzz
+        3 pick 3 pick > if s"  " s+ endif type
+        1 + print_fizz_buzz_values_range
+    endif
+;
+
+: print_fizz_buzz_values ( n -- )
+    s\" fizz buzz values:\n" type
+    1 print_fizz_buzz_values_range
+    s\" \n" type
+;
+
