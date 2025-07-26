3525d0d8a3dd294202630769e7b9e27ff568f69e
SUZUKI Kei <todokimasen@gmail.com>
2025-07-27 08:31:43 +0900

    random.haskell

---
+++ workspace/2025-07-27/random.haskell/main.hs
@@ -0,0 +1,21 @@
+import Control.Monad (replicateM)
+import Control.Monad.State (runState)
+import Control.Monad.State (state)
+import System.Random (StdGen)
+import System.Random (getStdGen)
+import System.Random (randomR)
+
+main :: IO ()
+main = do
+    gen <- getStdGen
+    let (xs, gen') = randomRs (10, 99) 20 gen
+    print xs
+    print gen
+    print gen'
+
+randomRs :: (Int, Int) -> Int -> StdGen -> ([Int], StdGen)
+randomRs range n gen = runState m gen
+    where
+        s = state (randomR range)
+        m = replicateM n s
+
