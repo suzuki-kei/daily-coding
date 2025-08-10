cfce59101af2d2a17290a55bc730a7991acfe846
SUZUKI Kei <todokimasen@gmail.com>
2025-08-11 08:28:00 +0900

    set.shell

--- workspace/2025-08-11/set.shell/main.sh
+++ workspace/2025-08-11/set.shell/main.sh
@@ -6,12 +6,12 @@ function main
 {
     echo "set1 = $(set1 | to_line)"
     echo "set2 = $(set2 | to_line)"
-    echo "union(set1, set2) = $(sort -u <(set1) <(set2) | to_line)"
-    echo "union(set2, set1) = $(sort -u <(set2) <(set1) | to_line)"
-    echo "difference(set1, set2) = $(sort <(set1) <(set2) <(set2) | uniq -u | to_line)"
-    echo "difference(set2, set1) = $(sort <(set2) <(set1) <(set1) | uniq -u | to_line)"
-    echo "intersection(set1, set2) = $(sort <(set1) <(set2) | uniq -d | to_line)"
-    echo "intersection(set2, set1) = $(sort <(set2) <(set1) | uniq -d | to_line)"
+    echo "union(set1, set2) = $(sort -nu <(set1) <(set2) | to_line)"
+    echo "union(set2, set1) = $(sort -nu <(set2) <(set1) | to_line)"
+    echo "difference(set1, set2) = $(sort -n <(set1) <(set2) <(set2) | uniq -u | to_line)"
+    echo "difference(set2, set1) = $(sort -n <(set2) <(set1) <(set1) | uniq -u | to_line)"
+    echo "intersection(set1, set2) = $(sort -n <(set1) <(set2) | uniq -d | to_line)"
+    echo "intersection(set2, set1) = $(sort -n <(set2) <(set1) | uniq -d | to_line)"
 }
 
 function set1
