f4f0d3b55756c0213ee6e5f817f4e684ec829c00
SUZUKI Kei <todokimasen@gmail.com>
2025-08-17 13:26:43 +0900

    bit-count.c

---
+++ workspace/2025-08-17/bit-count.c/main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+void demonstration(void);
+int bit_count(unsigned int x);
+
+int main(void)
+{
+    demonstration();
+
+    return 0;
+}
+
+void demonstration(void)
+{
+    int i;
+
+    for (i = 0; i <= 32; i++)
+        printf("bit_count(%2u) = %d\n", i, bit_count(i));
+}
+
+int bit_count(unsigned int x)
+{
+    x = (x & 0x55555555) + ((x >>  1) & 0x55555555);
+    x = (x & 0x33333333) + ((x >>  2) & 0x33333333);
+    x = (x & 0x0F0F0F0F) + ((x >>  4) & 0x0F0F0F0F);
+    x = (x & 0x00FF00FF) + ((x >>  8) & 0x00FF00FF);
+    x = (x & 0x0000FFFF) + ((x >> 16) & 0x0000FFFF);
+    return x;
+}
+
