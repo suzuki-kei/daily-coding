18885e8fe195a85aaf146bf4dbacd2c090289f17
SUZUKI Kei <todokimasen@gmail.com>
2025-12-09 00:33:57 +0900

    quick-sort.java

---
+++ workspace/2025-12-08/quick-sort.java/main.java
@@ -0,0 +1,138 @@
+import java.util.Arrays;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+class Application
+{
+
+    public static void main(String[] arguments)
+    {
+        demonstration("partition 2-way", new Partition2way());
+        demonstration("partition 3-way", new Partition3way());
+    }
+
+    private record PartitionResult(int leftUpper, int rightLower)
+    {
+    }
+
+    @FunctionalInterface
+    private interface Partition
+    {
+        public PartitionResult apply(int[] array, int lower, int upper);
+    }
+
+    private static void demonstration(String label, Partition partition)
+    {
+        System.out.printf("==== %s\n", label);
+
+        int[] array = generateRandomValues(20);
+        printArray(array);
+        quickSort(array, partition);
+        printArray(array);
+    }
+
+    private static int[] generateRandomValues(int n)
+    {
+        return IntStream.range(0, n)
+                .map(_ -> (int) (Math.random() * 90) + 10)
+                .toArray();
+    }
+
+    private static void printArray(int[] array)
+    {
+        String formatted = Arrays.stream(array)
+                .mapToObj(String::valueOf)
+                .collect(Collectors.joining(" "));
+
+        if (isSorted(array))
+            System.out.printf("%s (sorted)\n", formatted);
+        else
+            System.out.printf("%s (not sorted)\n", formatted);
+    }
+
+    private static boolean isSorted(int[] array)
+    {
+        for (int i = 0; i < array.length - 1; i++)
+            if (array[i] > array[i + 1])
+                return false;
+
+        return true;
+    }
+
+    private static void quickSort(int[] array, Partition partition)
+    {
+        if (array.length <= 1)
+            return;
+
+        quickSort(array, 0, array.length - 1, partition);
+    }
+
+    private static void quickSort(int[] array, int lower, int upper, Partition partition)
+    {
+        if (lower >= upper)
+            return;
+
+        final PartitionResult result = partition.apply(array, lower, upper);
+        quickSort(array, lower, result.leftUpper(), partition);
+        quickSort(array, result.rightLower(), upper, partition);
+    }
+
+    private static class Partition2way implements Partition
+    {
+        public PartitionResult apply(int[] array, int lower, int upper)
+        {
+            int incrementIndex = lower;
+            int decrementIndex = upper;
+            final int pivot = randomSelect(array, lower, upper);
+
+            while (incrementIndex <= decrementIndex)
+            {
+                while (array[incrementIndex] < pivot)
+                    incrementIndex++;
+                while (array[decrementIndex] > pivot)
+                    decrementIndex--;
+                if (incrementIndex <= decrementIndex)
+                    swap(array, incrementIndex++, decrementIndex--);
+            }
+
+            return new PartitionResult(decrementIndex, incrementIndex);
+        }
+    }
+
+    private static class Partition3way implements Partition
+    {
+        public PartitionResult apply(int[] array, int lower, int upper)
+        {
+            int lessEnd = lower;
+            int incrementIndex = lower;
+            int decrementIndex = upper;
+            final int pivot = randomSelect(array, lower, upper);
+
+            while (incrementIndex <= decrementIndex)
+            {
+                if (array[incrementIndex] < pivot)
+                    swap(array, lessEnd++, incrementIndex++);
+                else if (array[incrementIndex] > pivot)
+                    swap(array, incrementIndex, decrementIndex--);
+                else
+                    incrementIndex++;
+            }
+
+            return new PartitionResult(lessEnd - 1, incrementIndex);
+        }
+    }
+
+    private static int randomSelect(int[] array, int lower, int upper)
+    {
+        return array[(int) (Math.random() * (upper - lower + 1)) + lower];
+    }
+
+    private static void swap(int[] array, int index1, int index2)
+    {
+        int temporary = array[index1];
+        array[index1] = array[index2];
+        array[index2] = temporary;
+    }
+
+}
+
