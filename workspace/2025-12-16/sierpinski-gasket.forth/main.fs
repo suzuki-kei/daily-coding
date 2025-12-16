60f4721743527f56c017534cda3182169921c9e8
SUZUKI Kei <todokimasen@gmail.com>
2025-12-16 11:39:52 +0900

    sierpinski-gasket.forth

---
+++ workspace/2025-12-16/sierpinski-gasket.forth/main.fs
@@ -0,0 +1,33 @@
+
+: print_sierpinski_gasket_row recursive ( width y x -- )
+    2 pick over = if
+        drop
+        s\" \n" type
+    else
+        over over and 0 = if
+            s" ＊" type
+        else
+            s" 　" type
+        endif
+        1 + recurse
+    endif
+;
+
+: print_sierpinski_gasket_rows recursive ( height width y -- )
+    2 pick over = if
+        drop drop drop
+    else
+        0 print_sierpinski_gasket_row
+        1 + recurse
+    endif
+;
+
+: print_sierpinski_gasket ( height width -- )
+    0 print_sierpinski_gasket_rows
+;
+
+: main ( -- )
+    32 32 print_sierpinski_gasket
+    bye
+;
+
