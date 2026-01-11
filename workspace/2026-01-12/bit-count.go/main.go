6d1ff723743052d5bb018ece2b19f70062988bae
SUZUKI Kei <todokimasen@gmail.com>
2026-01-12 07:34:37 +0900

    bit-count.go

---
+++ workspace/2026-01-12/bit-count.go/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+    for i := 0; i <= 32; i++ {
+        fmt.Printf("bitCount(%2d) = %d\n", i, bitCount(uint(i)))
+    }
+}
+
+func bitCount(n uint) int {
+    n = (n & 0x55555555) + ((n >>  1) & 0x55555555)
+    n = (n & 0x33333333) + ((n >>  2) & 0x33333333)
+    n = (n & 0x0F0F0F0F) + ((n >>  4) & 0x0F0F0F0F)
+    n = (n & 0x00FF00FF) + ((n >>  8) & 0x00FF00FF)
+    n = (n & 0x0000FFFF) + ((n >> 16) & 0x0000FFFF)
+    return int(n)
+}
+
