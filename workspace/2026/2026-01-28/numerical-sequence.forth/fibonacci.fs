311d32f385c8a323c55b0232ef37724aac6dce0c
SUZUKI Kei <todokimasen@gmail.com>
2026-01-28 07:46:13 +0900

    numerical-sequence.forth

---
+++ workspace/2026/2026-01-28/numerical-sequence.forth/fibonacci.fs
@@ -0,0 +1,35 @@
+
+: _fibonacci ( a b n -- fn )
+    dup 0 = if
+        drop drop   ( a b n -- fn=a )
+    else
+        rot rot     ( a b n -- n a b )
+        tuck + rot  ( n a b -- b a+b n )
+        1 -         ( b a+b n -- b a+b n-1 )
+        recurse
+    endif
+;
+
+: fibonacci ( n -- fn )
+    0 1 rot     ( n -- a b n )
+    _fibonacci  ( a b n -- fn )
+;
+
+: print_fibonacci_numbers_range ( max min -- )
+    over over < if
+        drop drop   ( max min -- )
+    else
+        dup         ( max min -- max min n )
+        fibonacci . ( max min n -- max min )
+        1 +         ( max min -- max min+1 )
+        recurse
+    endif
+;
+
+: print_fibonacci_numbers ( n -- )
+    s\" fibonacci numbers:\n" type
+    0                               ( n -- max=n min )
+    print_fibonacci_numbers_range   ( max=n min -- )
+    s\" \n" type
+;
+
