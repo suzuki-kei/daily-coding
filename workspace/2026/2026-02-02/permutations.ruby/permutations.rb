78cedd0fcd6d8fbf6184c8ead4dfb53c61064c54
SUZUKI Kei <todokimasen@gmail.com>
2026-02-02 22:36:39 +0900

    permutations.ruby

---
+++ workspace/2026/2026-02-02/permutations.ruby/permutations.rb
@@ -0,0 +1,31 @@
+
+def permutations(xs)
+    enumerate_permutations(xs).to_a
+end
+
+def enumerate_permutations(xs)
+    Enumerator.new do |yielder|
+        generate_permutations(xs, &yielder.method(:yield))
+    end
+end
+
+def generate_permutations(xs, &block)
+    generate_permutations!(xs.clone, xs.size, &block)
+end
+
+def generate_permutations!(xs, n, &block)
+    if n == 1
+        block.call(xs.clone)
+    else
+        (0...n).each do |i|
+            generate_permutations!(xs, n - 1, &block)
+
+            if n.odd?
+                xs[0], xs[n - 1] = xs[n - 1], xs[0]
+            else
+                xs[i], xs[n - 1] = xs[n - 1], xs[i]
+            end
+        end
+    end
+end
+
