bf668bed545f19682ebee18f3125c3961f294237
SUZUKI Kei <todokimasen@gmail.com>
2026-02-12 07:52:04 +0900

    merge-sort.scheme

---
+++ workspace/2026/2026-02-12/merge-sort.scheme/main.scm
@@ -0,0 +1,119 @@
+(use srfi-13)
+(use srfi-27)
+
+(define main
+    (lambda (_)
+        (initialize)
+        (demonstration)
+        0))
+
+(define initialize
+    (lambda ()
+        (random-source-randomize! default-random-source)))
+
+(define demonstration
+    (lambda ()
+        (let ((xs (generate-random-values 20)))
+            (print-xs xs)
+            (print-xs (merge-sort xs)))))
+
+(define generate-random-values
+    (lambda (n)
+        (map
+            (lambda (_)
+                (+
+                    (random-integer 90)
+                    10))
+            (iota n))))
+
+(define print-xs
+    (lambda (xs)
+        (cond
+            ((sorted? xs)
+                (display
+                    (format
+                        "~a (sorted)~%"
+                        (xs->string " " xs))))
+            (else
+                (display
+                    (format
+                        "~a (not sorted)~%"
+                        (xs->string " " xs)))))))
+
+(define sorted?
+    (lambda (xs)
+        (cond
+            ((null? xs)
+                #t)
+            ((null? (cdr xs))
+                #t)
+            ((> (car xs) (cadr xs))
+                #f)
+            (else
+                (sorted? (cdr xs))))))
+
+(define xs->string
+    (lambda (separator xs)
+        (string-concatenate
+            (map
+                x->string
+                (intersperse separator xs)))))
+
+(define merge-sort
+    (lambda (xs)
+        (cond
+            ((null? xs)
+                '())
+            ((null? (cdr xs))
+                (list (car xs)))
+            (else
+                (receive
+                    (left-xs right-xs)
+                    (split xs)
+                    (merge
+                        (merge-sort left-xs)
+                        (merge-sort right-xs)))))))
+
+(define split
+    (lambda (xs)
+        (define split
+            (lambda (slow fast xs)
+                (cond
+                    ((null? fast)
+                        (values
+                            (reverse xs)
+                            slow))
+                    ((null? (cdr fast))
+                        (split
+                            (cdr slow)
+                            (cdr fast)
+                            (cons (car slow) xs)))
+                    (else
+                        (split
+                            (cdr slow)
+                            (cddr fast)
+                            (cons (car slow) xs))))))
+        (split xs xs '())))
+
+(define merge
+    (lambda (xs ys)
+        (define merge
+            (lambda (xs ys merged)
+                (cond
+                    ((or (null? xs) (null? ys))
+                        (append
+                            (reverse merged)
+                            xs
+                            ys))
+                    ((<= (car xs) (car ys))
+                        (merge
+                            (cdr xs)
+                            ys
+                            (cons (car xs) merged)))
+                    (else
+                        (merge
+                            xs
+                            (cdr ys)
+                            (cons (car ys) merged))))))
+        (merge xs ys '())))
+
