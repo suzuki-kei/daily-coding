1673776a3e75090843ce751aa3aaa2fb810af477
SUZUKI Kei <todokimasen@gmail.com>
2026-02-20 08:06:15 +0900

    insertion-sort.c

---
+++ workspace/2026/2026-02-20/insertion-sort.c/main.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define ARRAY_LENGTH(array) \
+    (sizeof(array) / sizeof(*array))
+
+void initialize(void);
+void demonstration(void);
+void set_random_values(int *array, int n);
+void print_array(const int *array, int n);
+int is_sorted(const int *array, int n);
+void insertion_sort(int *array, int n);
+void swap(int *value1, int *value2);
+
+int main(void)
+{
+    initialize();
+    demonstration();
+
+    return 0;
+}
+
+void initialize(void)
+{
+    srand(time(NULL));
+}
+
+void demonstration(void)
+{
+    int array[20];
+
+    set_random_values(array, ARRAY_LENGTH(array));
+    print_array(array, ARRAY_LENGTH(array));
+    insertion_sort(array, ARRAY_LENGTH(array));
+    print_array(array, ARRAY_LENGTH(array));
+}
+
+void set_random_values(int *array, int n)
+{
+    for (int i = 0; i < n; i++)
+        array[i] = rand() % 90 + 10;
+}
+
+void print_array(const int *array, int n)
+{
+    const char *separator = "";
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("%s%d", separator, array[i]);
+        separator = " ";
+    }
+
+    if (is_sorted(array, n))
+        printf(" (sorted)\n");
+    else
+        printf(" (not sorted)\n");
+}
+
+int is_sorted(const int *array, int n)
+{
+    for (int i = 0; i < n - 1; i++)
+        if (array[i] > array[i + 1])
+            return 0;
+
+    return 1;
+}
+
+void insertion_sort(int *array, int n)
+{
+    for (int sorted_size = 1; sorted_size < n; sorted_size++)
+    {
+        int insertion_index = sorted_size;
+        int insertion_value = array[sorted_size];
+
+        while (insertion_index > 0 && insertion_value < array[insertion_index - 1])
+        {
+            array[insertion_index] = array[insertion_index - 1];
+            insertion_index--;
+        }
+
+        array[insertion_index] = insertion_value;
+    }
+}
+
