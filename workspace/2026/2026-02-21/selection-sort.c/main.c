c25788af83e4468744551d629da2b802db1b7629
SUZUKI Kei <todokimasen@gmail.com>
2026-02-21 16:30:47 +0900

    selection-sort.c

---
+++ workspace/2026/2026-02-21/selection-sort.c/main.c
@@ -0,0 +1,90 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define ARRAY_LENGTH(array) \
+    (sizeof(array) / sizeof(*array))
+
+void initialize(void);
+void demonstration(void);
+void set_random_values(int *array, int n);
+void print_array(const int *array, int n);
+int is_sorted(const int *array, int n);
+void selection_sort(int *array, int n);
+void swap(int *value1, int *value2);
+
+int main(void)
+{
+    initialize();
+    demonstration();
+
+    return 0;
+}
+
+void initialize(void)
+{
+    srand(time(NULL));
+}
+
+void demonstration(void)
+{
+    int array[20];
+
+    set_random_values(array, ARRAY_LENGTH(array));
+    print_array(array, ARRAY_LENGTH(array));
+    selection_sort(array, ARRAY_LENGTH(array));
+    print_array(array, ARRAY_LENGTH(array));
+}
+
+void set_random_values(int *array, int n)
+{
+    for (int i = 0; i < n; i++)
+        array[i] = rand() % 90 + 10;
+}
+
+void print_array(const int *array, int n)
+{
+    const char *separator = "";
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("%s%d", separator, array[i]);
+        separator = " ";
+    }
+
+    if (is_sorted(array, n))
+        printf(" (sorted)\n");
+    else
+        printf(" (not sorted)\n");
+}
+
+int is_sorted(const int *array, int n)
+{
+    for (int i = 0; i < n - 1; i++)
+        if (array[i] > array[i + 1])
+            return 0;
+
+    return 1;
+}
+
+void selection_sort(int *array, int n)
+{
+    for (int unsorted_size = n; unsorted_size > 0; unsorted_size--)
+    {
+        int maxinum_index = 0;
+
+        for (int i = 1; i < unsorted_size; i++)
+            if (array[i] > array[maxinum_index])
+                maxinum_index = i;
+
+        swap(&array[maxinum_index], &array[unsorted_size - 1]);
+    }
+}
+
+void swap(int *value1, int *value2)
+{
+    const int temporary = *value1;
+    *value1 = *value2;
+    *value2 = temporary;
+}
+
