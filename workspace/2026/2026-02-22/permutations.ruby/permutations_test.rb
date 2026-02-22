a4499df78687841fc7d86de55a84ec83f1e05de8
SUZUKI Kei <todokimasen@gmail.com>
2026-02-22 14:32:43 +0900

    permutations.ruby

---
+++ workspace/2026/2026-02-22/permutations.ruby/permutations_test.rb
@@ -0,0 +1,33 @@
+require 'test/unit'
+require 'permutations'
+
+class PermutationsTestCase < Test::Unit::TestCase
+
+    def test_permutations
+        argument_expected_map = {
+            [] => [
+            ],
+            [1] => [
+                [1],
+            ],
+            [1, 2] => [
+                [1, 2],
+                [2, 1],
+            ],
+            [1, 2, 3] => [
+                [1, 2, 3],
+                [1, 3, 2],
+                [2, 1, 3],
+                [2, 3, 1],
+                [3, 1, 2],
+                [3, 2, 1],
+            ],
+        }
+
+        argument_expected_map.each do |argument, expected|
+            assert_equal expected.sort, permutations(argument).sort
+        end
+    end
+
+end
+
