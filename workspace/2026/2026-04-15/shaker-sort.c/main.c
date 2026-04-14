1399f0cc8f59a5c8d4e2107d0ab0fb73acd0d355
SUZUKI Kei <todokimasen@gmail.com>
2026-04-15 06:52:48 +0900

    shaker-sort.c

---
+++ workspace/2026/2026-04-15/shaker-sort.c/main.c
@@ -0,0 +1,100 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define ARRAY_LENGTH(array) \
+    (sizeof(array) / sizeof(*array))
+
+void initialize(void);
+void demonstration(void);
+void set_random_values(int *array, int n);
+int random_range(int lower, int upper);
+void print_array(const int *array, int n);
+int is_sorted(const int *array, int n);
+void shaker_sort(int *array, int n);
+void swap(int *value1, int *value2);
+
+int main(void)
+{
+    initialize();
+    demonstration();
+
+    return 0;
+}
+
+void initialize(void)
+{
+    srand(time(NULL));
+}
+
+void demonstration(void)
+{
+    int array[20];
+    set_random_values(array, ARRAY_LENGTH(array));
+    print_array(array, ARRAY_LENGTH(array));
+    shaker_sort(array, ARRAY_LENGTH(array));
+    print_array(array, ARRAY_LENGTH(array));
+}
+
+void set_random_values(int *array, int n)
+{
+    for (int i = 0; i < n; i++)
+        array[i] = random_range(10, 99);
+}
+
+int random_range(int lower, int upper)
+{
+    return rand() % (upper - lower + 1) + lower;
+}
+
+void print_array(const int *array, int n)
+{
+    const char *separator = "";
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("%s%d", separator, array[i]);
+        separator = " ";
+    }
+
+    if (is_sorted(array, n))
+        printf(" (sorted)\n");
+    else
+        printf(" (not sorted)\n");
+}
+
+int is_sorted(const int *array, int n)
+{
+    for (int i = 0; i + 1 < n; i++)
+        if (array[i] > array[i + 1])
+            return 0;
+
+    return 1;
+}
+
+void shaker_sort(int *array, int n)
+{
+    int lower = 0;
+    int upper = n - 1;
+
+    while (lower < upper)
+    {
+        for (int i = lower; i + 1 <= upper; i++)
+            if (array[i] > array[i + 1])
+                swap(&array[i], &array[i + 1]);
+        upper--;
+
+        for (int i = upper; i - 1 >= lower; i--)
+            if (array[i] < array[i - 1])
+                swap(&array[i], &array[i - 1]);
+        lower++;
+    }
+}
+
+void swap(int *value1, int *value2)
+{
+    const int temporary = *value1;
+    *value1 = *value2;
+    *value2 = temporary;
+}
+
