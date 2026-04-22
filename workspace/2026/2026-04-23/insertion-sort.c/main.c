5451bdf9a14738001e5a043873bf3d3b7b25c599
SUZUKI Kei <todokimasen@gmail.com>
2026-04-23 07:06:24 +0900

    insertion-sort.c

---
+++ workspace/2026/2026-04-23/insertion-sort.c/main.c
@@ -0,0 +1,93 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define ARRAY_LENGTH(array) \
+    (sizeof(array) / sizeof(*array))
+
+void initialize(void);
+void demonstration(void);
+void set_random_values(int *array, int n);
+int random_range(int lower, int upper);
+void print_array(const int *array, int n);
+int is_sorted(const int *array, int n);
+void insertion_sort(int *array, int n);
+void insert(int *array, int i);
+
+int main(void)
+{
+    initialize();
+    demonstration();
+
+    return 0;
+}
+
+void initialize(void)
+{
+    srand(time(NULL));
+}
+
+void demonstration(void)
+{
+    int array[20];
+    set_random_values(array, ARRAY_LENGTH(array));
+    print_array(array, ARRAY_LENGTH(array));
+    insertion_sort(array, ARRAY_LENGTH(array));
+    print_array(array, ARRAY_LENGTH(array));
+}
+
+void set_random_values(int *array, int n)
+{
+    for (int i = 0; i < n; i++)
+        array[i] = random_range(10, 99);
+}
+
+int random_range(int lower, int upper)
+{
+    return rand() % (upper - lower + 1) + lower;
+}
+
+void print_array(const int *array, int n)
+{
+    const char *separator = "";
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("%s%d", separator, array[i]);
+        separator = " ";
+    }
+
+    if (is_sorted(array, n))
+        printf(" (sorted)\n");
+    else
+        printf(" (not sorted)\n");
+}
+
+int is_sorted(const int *array, int n)
+{
+    for (int i = 0; i + 1 < n; i++)
+        if (array[i] > array[i + 1])
+            return 0;
+
+    return 1;
+}
+
+void insertion_sort(int *array, int n)
+{
+    for (int i = 1; i < n; i++)
+        insert(array, i);
+}
+
+void insert(int *array, int i)
+{
+    const int insertion_value = array[i];
+
+    while (i >= 1 && insertion_value < array[i - 1])
+    {
+        array[i] = array[i - 1];
+        i--;
+    }
+
+    array[i] = insertion_value;
+}
+
