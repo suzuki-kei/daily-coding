41cac42ebb3259b6336890158532dea9c0cc6f8c
SUZUKI Kei <todokimasen@gmail.com>
2026-05-05 20:04:08 +0900

    run-length-encoding.haskell

--- workspace/2026/2026-05-05/run-length-encoding.haskell/RunLengthEncoding.hs
+++ workspace/2026/2026-05-05/run-length-encoding.haskell/RunLengthEncoding.hs
@@ -1,35 +1,50 @@
 module RunLengthEncoding where
 
 import Data.List (replicate)
-import Data.List.NonEmpty ((<|))
+import Data.List (unfoldr)
 import Data.List.NonEmpty (NonEmpty ((:|)))
 import Data.List.NonEmpty (NonEmpty)
+import Data.List.NonEmpty (toList)
 
 type Run a = NonEmpty a
 type EncodedRun a = (a, Int)
-type DecodedRun a = [a]
 
-encode :: Eq a => [a] -> [EncodedRun a]
-encode = map encodeRun . listToRuns
+encode ::
+    Eq a => [a] -> [EncodedRun a]
+encode =
+    map encodeRun . listToRuns
 
-encodeRun :: Run a -> EncodedRun a
-encodeRun run@(x :| _) = (x, length run)
+listToRuns ::
+    Eq a => [a] -> [Run a]
+listToRuns xs =
+    unfoldr step xs
+        where
+            step [] =
+                Nothing
+            step (x : xs) =
+                let
+                    (run', xs') = span (== x) xs
+                    run = (x :| run')
+                in
+                    Just (run, xs')
 
-listToRuns :: Eq a => [a] -> [Run a]
-listToRuns [] = []
-listToRuns (x : xs) = listToRuns' xs (x :| []) []
-    where
-        listToRuns' [] run runs = reverse (run : runs)
-        listToRuns' (x : xs) run@(runHead :| runTail) runs
-            | x == runHead = listToRuns' xs (x <| run) runs
-            | otherwise    = listToRuns' xs (x :| []) (run : runs)
+encodeRun ::
+    Run a -> EncodedRun a
+encodeRun xs@(x :| _) =
+    (x, length xs)
 
-decode :: [EncodedRun a] -> [a]
-decode = runsToList . map decodeRun
+decode ::
+    [EncodedRun a] -> [a]
+decode =
+    runsToList . map decodeRun
 
-decodeRun :: EncodedRun a -> DecodedRun a
-decodeRun (x, n) = replicate n x
+decodeRun ::
+    EncodedRun a -> Run a
+decodeRun (x, n) =
+    (x :| replicate (n - 1) x)
 
-runsToList :: [DecodedRun a] -> [a]
-runsToList runs = foldl (++) [] runs
+runsToList ::
+    [Run a] -> [a]
+runsToList runs =
+    foldl (++) [] $ map toList runs
 
