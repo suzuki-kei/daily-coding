3b2d9fa288ed3acc1a4513f287b37b1dc5313bdc
SUZUKI Kei <todokimasen@gmail.com>
2026-05-08 10:53:06 +0900

    run-length-encoding.haskell

---
+++ workspace/2026/2026-05-08/run-length-encoding.haskell/RunLengthEncoding.hs
@@ -0,0 +1,23 @@
+module RunLengthEncoding where
+
+import Data.List (replicate)
+import Data.List.NonEmpty (group)
+import Data.List.NonEmpty (NonEmpty ((:|)))
+import Data.List.NonEmpty (NonEmpty)
+import Data.List.NonEmpty (toList)
+
+type Run a = NonEmpty a
+type EncodedRun a = (a, Int)
+
+encode :: Eq a => [a] -> [EncodedRun a]
+encode = map encodeRun . listToRuns
+    where
+        listToRuns = group
+        encodeRun run@(x :| _) = (x, length run)
+
+decode :: [EncodedRun a] -> [a]
+decode = runsToList . map decodeRun
+    where
+        decodeRun (x, n) = x :| replicate (n - 1) x
+        runsToList = concat . map toList
+
