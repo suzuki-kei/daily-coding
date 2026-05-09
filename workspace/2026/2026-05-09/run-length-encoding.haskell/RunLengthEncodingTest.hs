ef7679f9a3f17bd136010d17150627b10dfff0ef
SUZUKI Kei <todokimasen@gmail.com>
2026-05-09 09:13:58 +0900

    run-length-encoding.haskell

---
+++ workspace/2026/2026-05-09/run-length-encoding.haskell/RunLengthEncodingTest.hs
@@ -0,0 +1,57 @@
+module RunLengthEncodingTest where
+
+import RunLengthEncoding (decode)
+import RunLengthEncoding (encode)
+import RunLengthEncoding (EncodedRun)
+import Test.HUnit ((~:))
+import Test.HUnit ((~=?))
+import Test.HUnit (runTestTTAndExit)
+import Test.HUnit (Test (TestList))
+import Test.HUnit (test)
+import Test.HUnit (Test)
+import Text.Printf (printf)
+
+main :: IO ()
+main = runTestTTAndExit allTests
+
+allTests :: Test
+allTests = TestList [
+    encodeTests,
+    decodeTests]
+
+type TestDataTuple = (String, [EncodedRun Char])
+
+testDataTuples :: [TestDataTuple]
+testDataTuples = [
+    ("",       []),
+    ("a",      [('a', 1)]),
+    ("aa",     [('a', 2)]),
+    ("aaa",    [('a', 3)]),
+    ("ab",     [('a', 1), ('b', 1)]),
+    ("abb",    [('a', 1), ('b', 2)]),
+    ("abba",   [('a', 1), ('b', 2), ('a', 1)]),
+    ("aabccc", [('a', 2), ('b', 1), ('c', 3)])]
+
+testsWithTestDataTuples :: (String -> [EncodedRun Char] -> Test) -> Test
+testsWithTestDataTuples assert =
+    let
+        labels = map mapper ns
+            where
+                ns = [1..] :: [Int]
+                mapper n = printf "#%d" n
+        tests = zipWith zipper labels testDataTuples
+            where
+                zipper label (xs, encodedRuns) = label ~: assert xs encodedRuns
+    in
+        test tests
+
+encodeTests :: Test
+encodeTests = testsWithTestDataTuples assert
+    where
+        assert xs encodedRuns = encodedRuns ~=? encode xs
+
+decodeTests :: Test
+decodeTests = testsWithTestDataTuples assert
+    where
+        assert xs encodedRuns = xs ~=? decode encodedRuns
+
