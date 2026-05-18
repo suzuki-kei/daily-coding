90e746650e6026f0e5111d1e007776b84e0b0d1b
SUZUKI Kei <todokimasen@gmail.com>
2026-05-18 21:39:33 +0900

    bubble-sort.go

---
+++ workspace/2026/2026-05-18/bubble-sort.go/main.go
@@ -0,0 +1,78 @@
+package main
+
+import "cmp"
+import "fmt"
+import "math/rand"
+import "strings"
+
+func main() {
+    xs := generateRandomValues(20)
+    printSlice(xs)
+    bubbleSort(xs)
+    printSlice(xs)
+}
+
+func generateRandomValues(n int) []int {
+    xs := make([]int, 0, n)
+
+    for i := 0; i < n; i++ {
+        xs = append(xs, randomRange(10, 99))
+    }
+
+    return xs
+}
+
+func printSlice(xs []int) {
+    s := strings.Join(mapSlice(toString, xs), " ")
+
+    if isSorted(xs) {
+        fmt.Printf("%s (sorted)\n", s)
+    } else {
+        fmt.Printf("%s (not sorted)\n", s)
+    }
+}
+
+func mapSlice[T any, U any](f func(T) U, xs []T) []U {
+    ys := make([]U, len(xs))
+
+    for i, x := range xs {
+        ys[i] = f(x)
+    }
+
+    return ys
+}
+
+func toString[T any](x T) string {
+    return fmt.Sprintf("%v", x)
+}
+
+func isSorted[T cmp.Ordered](xs []T) bool {
+    for i := 0; i + 1 < len(xs); i++ {
+        if xs[i] > xs[i + 1] {
+            return false
+        }
+    }
+
+    return true
+}
+
+func bubbleSort[T cmp.Ordered](xs []T) {
+    for last := len(xs) - 1; last >= 1; last-- {
+        for i := 0; i + 1 <= last; i++ {
+            if xs[i] > xs[i + 1] {
+                swap(&xs[i], &xs[i + 1])
+            }
+        }
+    }
+}
+
+func randomRange(min int, max int) int {
+    return rand.Intn(max - min + 1) + min
+}
+
+func swap[T any](x1 *T, x2 *T) {
+    temporary := *x1
+    *x1 = *x2
+    *x2 = temporary
+}
+
