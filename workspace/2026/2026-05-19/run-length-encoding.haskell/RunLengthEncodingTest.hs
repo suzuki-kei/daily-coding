ed0c4b8531bd94e1f3b0d43e82ece6984d64927e
SUZUKI Kei <todokimasen@gmail.com>
2026-05-19 07:47:48 +0900

    run-length-encoding.haskell

---
+++ workspace/2026/2026-05-19/run-length-encoding.haskell/RunLengthEncodingTest.hs
@@ -0,0 +1,59 @@
+module RunLengthEncodingTest where
+
+import RunLengthEncoding (decode)
+import RunLengthEncoding (encode)
+import RunLengthEncoding (EncodedRun)
+import Test.HUnit ((~:))
+import Test.HUnit ((~?=))
+import Test.HUnit (runTestTTAndExit)
+import Test.HUnit (Test (TestList))
+import Test.HUnit (test)
+import Test.HUnit (Test)
+import Text.Printf (printf)
+
+type TestData = (String, [EncodedRun Char])
+
+main :: IO ()
+main = runTestTTAndExit allTests
+
+allTests :: Test
+allTests = TestList [
+    encodeTests,
+    decodeTests]
+
+testDataList :: [TestData]
+testDataList = [
+    ("",       []),
+    ("a",      [('a', 1)]),
+    ("aa",     [('a', 2)]),
+    ("aaa",    [('a', 3)]),
+    ("ab",     [('a', 1), ('b', 1)]),
+    ("abb",    [('a', 1), ('b', 2)]),
+    ("abba",   [('a', 1), ('b', 2), ('a', 1)]),
+    ("aabccc", [('a', 2), ('b', 1), ('c', 3)])]
+
+labelledTests :: String -> [Test] -> [Test]
+labelledTests label assertions = zipWith (~:) labels assertions
+    where
+        labels = map mapper [1..]
+            where
+                mapper :: Int -> String
+                mapper n = printf "%s #%d" label n
+
+testWithTestDataList :: String -> (TestData -> Test) -> Test
+testWithTestDataList label assert = test tests
+    where
+        tests = labelledTests label assertions
+            where
+                assertions = map assert testDataList
+
+encodeTests :: Test
+encodeTests = testWithTestDataList "encode" assert
+    where
+        assert (xs, encodedRuns) = encode xs ~?= encodedRuns
+
+decodeTests :: Test
+decodeTests = testWithTestDataList "decode" assert
+    where
+        assert (xs, encodedRuns) = decode encodedRuns ~?= xs
+
