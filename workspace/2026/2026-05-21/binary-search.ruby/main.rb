12a76c68ed20ece27daecc9a17e4381cfc1e1a57
SUZUKI Kei <todokimasen@gmail.com>
2026-05-21 07:47:48 +0900

    binary-search.ruby

---
+++ workspace/2026/2026-05-21/binary-search.ruby/main.rb
@@ -0,0 +1,42 @@
+
+def main
+    values = generate_random_ascending_values(20)
+    puts values.join(' ')
+
+    lower = values[0] - 1
+    upper = values[-1] + 1
+
+    (lower..upper).each do |target|
+        index = binary_search(values, target)
+        puts "target=#{target}, index=#{index}"
+    end
+end
+
+def generate_random_ascending_values(n)
+    n.times.reduce([]) do |values, _|
+        offset = values[-1] || 9
+        values << rand(1..5) + offset
+    end
+end
+
+def binary_search(values, target, lower = 0, upper = values.size - 1)
+    if lower > upper
+        -1
+    else
+        center = (lower + upper) / 2
+
+        case
+            when target < values[center]
+                binary_search(values, target, lower, center - 1)
+            when target > values[center]
+                binary_search(values, target, center + 1, upper)
+            else
+                center
+        end
+    end
+end
+
+if $0 == __FILE__
+    main
+end
+
