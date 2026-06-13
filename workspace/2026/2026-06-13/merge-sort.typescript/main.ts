b6d1ef31391fed97f8ecd2c1b6922789c1b99b17
SUZUKI Kei <todokimasen@gmail.com>
2026-06-13 16:37:53 +0900

    merge-sort.typescript

---
+++ workspace/2026/2026-06-13/merge-sort.typescript/main.ts
@@ -0,0 +1,89 @@
+
+function main(): void
+{
+    const array = generate_random_values(20)
+    print_array(array)
+    merge_sort(array)
+    print_array(array)
+}
+
+function generate_random_values(n: number): number[]
+{
+    return Array.from({length: n}, () => random_range(10, 99))
+}
+
+function random_range(min: number, max: number): number
+{
+    return Math.floor(Math.random() * (max - min + 1)) + min
+}
+
+function print_array(array: readonly number[]): void
+{
+    if (is_sorted(array))
+        console.log(`${array.join(" ")} (sorted)`)
+    else
+        console.log(`${array.join(" ")} (not sorted)`)
+}
+
+function is_sorted(array: readonly number[]): boolean
+{
+    for (let i = 0; i + 1 < array.length; i++)
+        if (array[i] > array[i + 1])
+            return false
+
+    return true
+}
+
+function merge_sort(array: number[]): void
+{
+    const buffer = Array<number>(array.length)
+    let input = array
+    let output = buffer
+
+    for (let chunk_size = 1; chunk_size < array.length; chunk_size *= 2)
+    {
+        for (let i = 0; i < array.length; i += chunk_size * 2)
+        {
+            merge(
+                output,
+                i,
+                input,
+                i,
+                Math.min(array.length, i + chunk_size),
+                Math.min(array.length, i + chunk_size),
+                Math.min(array.length, i + chunk_size * 2))
+        }
+        [input, output] = [output, input]
+    }
+
+    if (array == output)
+        for (let i = 0; i < array.length; i++)
+            array[i] = buffer[i]
+}
+
+function merge(
+        output: number[],
+        output_index: number,
+        input: readonly number[],
+        begin1: number,
+        end1: number,
+        begin2: number,
+        end2: number): void
+{
+    while (begin1 !== end1 && begin2 !== end2)
+    {
+        if (input[begin1] <= input[begin2])
+            output[output_index++] = input[begin1++]
+        else
+            output[output_index++] = input[begin2++]
+    }
+
+    while (begin1 !== end1)
+        output[output_index++] = input[begin1++]
+
+    while (begin2 !== end2)
+        output[output_index++] = input[begin2++]
+}
+
+main()
+
