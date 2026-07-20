c1a3bad2bb2d3a31ad2188bdc6713a9ece01b5a0
SUZUKI Kei <todokimasen@gmail.com>
2026-07-21 07:34:44 +0900

    insertion-sort.typescript

---
+++ workspace/2026/2026-07-21/insertion-sort.typescript/main.ts
@@ -0,0 +1,61 @@
+
+function main(): void
+{
+    const array = generate_random_values(20)
+    print_array(array)
+    insertion_sort(array)
+    print_array(array)
+}
+
+function generate_random_values(n: number): number[]
+{
+    return new_array(n, _ => random_range(10, 99))
+}
+
+function new_array<T>(n: number, f: (i: number) => T): T[]
+{
+    return Array.from({length: n}, f)
+}
+
+function random_range(min: number, max: number): number
+{
+    return Math.floor(Math.random() * (max - min + 1)) + min
+}
+
+function print_array(array: readonly number[]): void
+{
+    if (is_sorted(array))
+        console.log(`${array.join(" ")} (sorted)`)
+    else
+        console.log(`${array.join(" ")} (not sorted)`)
+}
+
+function is_sorted(array: readonly number[]): boolean
+{
+    for (let i = 0; i + 1 < array.length; i++)
+        if (array[i] > array[i + 1])
+            return false
+
+    return true
+}
+
+function insertion_sort(array: number[]): void
+{
+    for (let i = 1; i < array.length; i++)
+        insert(array, i)
+}
+
+function insert(array: number[], i: number): void
+{
+    const value = array[i]
+
+    while (i >= 1 && value < array[i - 1])
+    {
+        array[i] = array[i - 1]
+        i--
+    }
+    array[i] = value
+}
+
+main()
+
