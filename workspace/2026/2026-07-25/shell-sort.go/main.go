db3b8849a3d78434a654676e2349b3825e8b7c35
SUZUKI Kei <todokimasen@gmail.com>
2026-07-25 17:22:52 +0900

    shell-sort.go

---
+++ workspace/2026/2026-07-25/shell-sort.go/main.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+    "cmp"
+    "fmt"
+    "math/rand"
+    "strings"
+)
+
+func main() {
+    xs := generateRandomValues(20)
+    printSlice(xs)
+    shellSort(xs)
+    printSlice(xs)
+}
+
+func generateRandomValues(n int) []int {
+    xs := make([]int, n)
+
+    for i := 0; i < n; i++ {
+        xs[i] = randomRange(10, 99)
+    }
+
+    return xs
+}
+
+func randomRange(min int, max int) int {
+    return rand.Intn(max - min + 1) + min
+}
+
+func printSlice[T cmp.Ordered](xs []T) {
+    s := strings.Join(mapSlice(toString, xs), " ")
+
+    if isSorted(xs) {
+        fmt.Printf("%s (sorted)\n", s)
+    } else {
+        fmt.Printf("%s (not sorted)\n", s)
+    }
+}
+
+func toString[T any](x T) string {
+    return fmt.Sprintf("%v", x)
+}
+
+func mapSlice[T any, U any](f func(T) U, xs []T) []U {
+    ys := make([]U, len(xs))
+
+    for i, x := range xs {
+        ys[i] = f(x)
+    }
+
+    return ys
+}
+
+func isSorted[T cmp.Ordered](xs []T) bool {
+    for i := 0; i + 1 < len(xs); i++ {
+        if xs[i] > xs[i + 1] {
+            return false
+        }
+    }
+
+    return true
+}
+
+func shellSort[T cmp.Ordered](xs []T) {
+    for gap := computeInitialGap(len(xs)); gap >= 1; gap /= 3 {
+        for i := gap; i < len(xs); i++ {
+            insert(xs, i, gap)
+        }
+    }
+}
+
+func computeInitialGap(n int) int {
+    gap := 1
+
+    for gap * 3 + 1 < n {
+        gap = gap * 3 + 1
+    }
+
+    return gap
+}
+
+func insert[T cmp.Ordered](xs []T, i int, gap int) {
+    x := xs[i]
+
+    for i >= gap && x < xs[i - gap] {
+        xs[i] = xs[i - gap]
+        i -= gap
+    }
+
+    xs[i] = x
+}
+
