10e84c64a0d16a43afc21c2756adc13c3bb7eb19
SUZUKI Kei <todokimasen@gmail.com>
2026-07-25 17:10:35 +0900

    quick-sort.go

---
+++ workspace/2026/2026-07-25/quick-sort.go/main.go
@@ -0,0 +1,154 @@
+package main
+
+import (
+    "cmp"
+    "fmt"
+    "math/rand"
+    "strings"
+)
+
+type PartitionResult struct {
+    LeftLast int
+    RightFirst int
+}
+
+type Partition[T cmp.Ordered] func(array []T, first int, last int) PartitionResult
+
+func main() {
+    demonstration("partition 2-way", partition2Way)
+    demonstration("partition 3-way", partition3Way)
+}
+
+func demonstration(label string, partition Partition[int]) {
+    fmt.Printf("==== %s\n", label)
+
+    xs := generateRandomValues(20)
+    printSlice(xs)
+    quickSort(xs, partition)
+    printSlice(xs)
+}
+
+func generateRandomValues(n int) []int {
+    xs := make([]int, n)
+
+    for i := 0; i < n; i++ {
+        xs[i] = randomRange(10, 99)
+    }
+
+    return xs
+}
+
+func randomRange(min int, max int) int {
+    return rand.Intn(max - min + 1) + min
+}
+
+func printSlice[T cmp.Ordered](xs []T) {
+    s := strings.Join(mapSlice(toString, xs), " ")
+
+    if isSorted(xs) {
+        fmt.Printf("%s (sorted)\n", s)
+    } else {
+        fmt.Printf("%s (not sorted)\n", s)
+    }
+}
+
+func toString[T any](x T) string {
+    return fmt.Sprintf("%v", x)
+}
+
+func mapSlice[T any, U any](f func(T) U, xs []T) []U {
+    ys := make([]U, len(xs))
+
+    for i, x := range xs {
+        ys[i] = f(x)
+    }
+
+    return ys
+}
+
+func isSorted[T cmp.Ordered](xs []T) bool {
+    for i := 0; i + 1 < len(xs); i++ {
+        if xs[i] > xs[i + 1] {
+            return false
+        }
+    }
+
+    return true
+}
+
+func quickSort[T cmp.Ordered](xs []T, partition Partition[T]) {
+    quickSortRange(xs, 0, len(xs) - 1, partition)
+}
+
+func quickSortRange[T cmp.Ordered](xs []T, first int, last int, partition Partition[T]) {
+    for first < last {
+        result := partition(xs, first, last)
+        leftSize := result.LeftLast - first + 1
+        rightSize := last - result.RightFirst + 1
+
+        if leftSize <= rightSize {
+            quickSortRange(xs, first, result.LeftLast, partition)
+            first = result.RightFirst
+        } else {
+            quickSortRange(xs, result.RightFirst, last, partition)
+            last = result.LeftLast
+        }
+    }
+}
+
+func partition2Way[T cmp.Ordered](xs []T, first int, last int) PartitionResult {
+    incrementIndex := first
+    decrementIndex := last
+    pivot := xs[randomRange(first, last)]
+
+    for incrementIndex <= decrementIndex {
+        for xs[incrementIndex] < pivot {
+            incrementIndex++
+        }
+
+        for xs[decrementIndex] > pivot {
+            decrementIndex--
+        }
+
+        if incrementIndex <= decrementIndex {
+            swap(&xs[incrementIndex], &xs[decrementIndex])
+            incrementIndex++
+            decrementIndex--
+        }
+    }
+
+    return PartitionResult {
+        LeftLast: decrementIndex,
+        RightFirst: incrementIndex,
+    }
+}
+
+func partition3Way[T cmp.Ordered](xs []T, first int, last int) PartitionResult {
+    lessEnd := first
+    incrementIndex := first
+    decrementIndex := last
+    pivot := xs[randomRange(first, last)]
+
+    for incrementIndex <= decrementIndex {
+        if xs[incrementIndex] < pivot {
+            swap(&xs[lessEnd], &xs[incrementIndex])
+            lessEnd++
+            incrementIndex++
+        } else if xs[incrementIndex] > pivot {
+            swap(&xs[incrementIndex], &xs[decrementIndex])
+            decrementIndex--
+        } else {
+            incrementIndex++
+        }
+    }
+
+    return PartitionResult {
+        LeftLast: lessEnd - 1,
+        RightFirst: incrementIndex,
+    }
+}
+
+func swap[T any](x1 *T, x2 *T) {
+    *x1, *x2 = *x2, *x1
+}
+
