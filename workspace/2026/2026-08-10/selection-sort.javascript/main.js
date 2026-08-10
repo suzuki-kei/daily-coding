48ee93a2f28ebdf0397c77c16a16e6d3cad21296
SUZUKI Kei <todokimasen@gmail.com>
2026-08-10 13:27:55 +0900

    selection-sort.javascript

---
+++ workspace/2026/2026-08-10/selection-sort.javascript/main.js
@@ -0,0 +1,60 @@
+
+const MIN = 10
+const MAX = 99
+
+function main()
+{
+    const array = generate_random_values(MIN, MAX, 20)
+    print_array(array)
+    selection_sort(array)
+    print_array(array)
+}
+
+function generate_random_values(min, max, n)
+{
+    return Array.from({length: n}, () => random_range(min, max))
+}
+
+function random_range(min, max)
+{
+    return Math.floor(Math.random() * (max - min + 1)) + min
+}
+
+function print_array(array)
+{
+    if (is_sorted(array))
+        console.log(`${array.join(" ")} (sorted)`)
+    else
+        console.log(`${array.join(" ")} (not sorted)`)
+}
+
+function is_sorted(array)
+{
+    for (let i = 0; i + 1 < array.length; i++)
+        if (array[i] > array[i + 1])
+            return false
+
+    return true
+}
+
+function selection_sort(array)
+{
+    for (let begin = 0; begin + 1 < array.length; begin++)
+    {
+        let minimum_index = begin
+
+        for (let i = begin + 1; i < array.length; i++)
+            if (array[i] < array[minimum_index])
+                minimum_index = i
+
+        swap(array, begin, minimum_index)
+    }
+}
+
+function swap(array, index1, index2)
+{
+    [array[index1], array[index2]] = [array[index2], array[index1]]
+}
+
+main()
+
