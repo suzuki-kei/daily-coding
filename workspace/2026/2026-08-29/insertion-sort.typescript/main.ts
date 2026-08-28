ace7779249e17353024e12aae4b13fe492d69756
SUZUKI Kei <todokimasen@gmail.com>
2026-08-29 07:33:36 +0900

    insertion-sort.typescript

---
+++ workspace/2026/2026-08-29/insertion-sort.typescript/main.ts
@@ -0,0 +1,55 @@
+
+function main(): void
+{
+    const array = generate_random_values(10, 99, 20)
+    print_array(array)
+    insertion_sort(array)
+    print_array(array)
+}
+
+function generate_random_values(min: number, max: number, n: number): number[]
+{
+    return Array.from({length: n}, () => random_range(min, max))
+}
+
+function random_range(min: number, max: number): number
+{
+    return Math.floor(Math.random() * (max - min + 1)) + min
+}
+
+function print_array(array: readonly number[]): void
+{
+    if (is_sorted(array))
+        console.log(`${array.join(" ")} (sorted)`)
+    else
+        console.log(`${array.join(" ")} (not sorted)`)
+}
+
+function is_sorted(array: readonly number[]): boolean
+{
+    for (let i = 0; i + 1 < array.length; i++)
+        if (array[i] > array[i + 1])
+            return false
+
+    return true
+}
+
+function insertion_sort(array: number[]): void
+{
+    for (let n_sorted = 1; n_sorted < array.length; n_sorted++)
+    {
+        let i = n_sorted
+        const value = array[i]
+
+        while (i >= 1 && value < array[i - 1])
+        {
+            array[i] = array[i - 1]
+            i--
+        }
+
+        array[i] = value
+    }
+}
+
+main()
+
