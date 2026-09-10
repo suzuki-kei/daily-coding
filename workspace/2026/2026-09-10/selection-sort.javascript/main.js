62a77b694cff88f0145e48b32e3990663d9a9be6
SUZUKI Kei <todokimasen@gmail.com>
2026-09-10 20:53:58 +0900

    selection-sort.javascript

---
+++ workspace/2026/2026-09-10/selection-sort.javascript/main.js
@@ -0,0 +1,58 @@
+
+function main()
+{
+    const array = generateRandomValues({ min: 10, max: 99, n: 20 })
+    printArray(array)
+    selectionSort(array)
+    printArray(array)
+}
+
+function generateRandomValues({ min, max, n })
+{
+    return Array.from({ length: n }, () => randomRange(min, max))
+}
+
+function randomRange(min, max)
+{
+    return Math.floor(Math.random() * (max - min + 1)) + min
+}
+
+function printArray(array)
+{
+    if (isSorted(array))
+        console.log(`${array.join(" ")} (sorted)`)
+    else
+        console.log(`${array.join(" ")} (not sorted)`)
+}
+
+function isSorted(array)
+{
+    return range(0, array.length - 2).every(i => array[i] <= array[i + 1]);
+}
+
+function range(min, max)
+{
+    return Array.from({ length: max - min + 1 }, (_, i) => i + min)
+}
+
+function selectionSort(array)
+{
+    for (let begin = 0; begin + 1 < array.length; begin++)
+    {
+        let minimumIndex = begin
+
+        for (let i = begin + 1; i < array.length; i++)
+            if (array[i] < array[minimumIndex])
+                minimumIndex = i
+
+        swap(array, begin, minimumIndex)
+    }
+}
+
+function swap(array, index1, index2)
+{
+    [array[index1], array[index2]] = [array[index2], array[index1]]
+}
+
+main()
+
