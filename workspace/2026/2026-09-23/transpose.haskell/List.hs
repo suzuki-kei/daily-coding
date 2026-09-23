5867552fa1f4b3ee02fbab82f015fc44ec726a86
SUZUKI Kei <todokimasen@gmail.com>
2026-09-23 13:28:27 +0900

    transpose.haskell

---
+++ workspace/2026/2026-09-23/transpose.haskell/List.hs
@@ -0,0 +1,13 @@
+module List where
+
+transpose ::
+    [[a]] -> [[a]]
+transpose xss =
+    let
+        (heads, tails) = unzip [(x, xs) | x : xs <- xss]
+    in
+        transpose' heads tails
+    where
+        transpose' [] [] = []
+        transpose' heads tails = heads : transpose tails
+
